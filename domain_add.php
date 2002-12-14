6beea6530fd4fd16b15f2042b1695489b29c8c3c
turbo <turbo>
2002-12-14 20:43:49 +0000

    * Replaced '<?php echo ...; ?>' with shorter '<?=...?>'. * Insert domain (default) values after creating the domain tree. This figured   out automaticly with the help of new user_generate_homedir() function which   resembles the old user_generate_mailstore().

--- domain_add.php
+++ domain_add.php
@@ -8,7 +8,7 @@ require("pql_control.inc");
 
 include("header.html");
 ?>
-  <span class="title1"><?php echo PQL_DOMAIN_ADD; ?>: <?php echo $domain ?></span>
+  <span class="title1"><?=PQL_DOMAIN_ADD?>: <?=$domain?></span>
   <br><br>
 <?php
 $_pql = new pql($USER_DN, $USER_PASS);
@@ -19,45 +19,76 @@ $domain = strtolower($domain);
 
 // Should we force a dot in the domainname or not?
 if(PQL_LDAP_OBJECTCLASS_DOMAIN == "domain") {
-    // We're using a domain object, which means we should allow
-    // a domain name to be without dot.
-    $nodot = 1;
+	// We're using a domain object, which means we should allow
+	// a domain name to be without dot.
+	$nodot = 1;
 } else {
-    $nodot = 0;
+	$nodot = 0;
 }
 
 // check if domain is valid
 if(!check_hostaddress($domain, $nodot)){
-    $msg = urlencode(PQL_DOMAIN_INVALID);
-    header("Location: " . PQL_URI . "home.php?msg=$msg");
-    exit();
+	$msg = urlencode(PQL_DOMAIN_INVALID);
+	header("Location: " . PQL_URI . "home.php?msg=$msg");
+	exit();
 }
 
 // check if domain exist
 if(pql_domain_exist($_pql->ldap_linkid, PQL_LDAP_BASEDN, $domain)){
-    $msg = urlencode(PQL_DOMAIN_EXISTS);
-    header("Location: " . PQL_URI . "home.php?msg=$msg");
-    exit();
+	$msg = urlencode(PQL_DOMAIN_EXISTS);
+	header("Location: " . PQL_URI . "home.php?msg=$msg");
+	exit();
 }
 
-if(pql_add_domain($_pql->ldap_linkid, PQL_LDAP_BASEDN, $domain)){
-    // NOTE: Don't execute this one if we're using 'domain' mode.
-    //       The FQDN isn't know yet!
-    if(PQL_LDAP_OBJECTCLASS_DOMAIN != "domain") {
+// Defaultvalues we can easily figure out
+$defaulthomedir = user_generate_homedir('', '', '', $domain, '');
+$defaultmaildir = user_generate_mailstore('', '', '', $domain, '');
+
+if(pql_add_domain($_pql->ldap_linkid, PQL_LDAP_BASEDN, $domain)) {
+	if($defaultdomain != "") {
 		// update locals if control patch is enabled
 		if(pql_control_update_domains($_pql->ldap_linkid, PQL_LDAP_BASEDN,
 									  $_pql_control->ldap_linkid,
 									  PQL_LDAP_CONTROL_BASEDN)) {
 			// message ??
 		}
-    }
+	}
 	
-    // redirect to domain-details
-    $msg = urlencode(pql_complete_constant(PQL_DOMAIN_ADD_OK, array("domain" => $domain)));
-    header("Location: " . PQL_URI . "domain_detail.php?domain=$domain&msg=$msg&rlnb=1");
+	$msg = "";
+	
+	// Save the attributes - Default domain
+	if($defaultdomain && !pql_set_domain_value($_pql->ldap_linkid, $domain, 'defaultDomain', $defaultdomain)) {
+		$msg = PQL_DOMAIN_DEFAULT_CHANGE_FAILED . ":&nbsp;" . ldap_error($_pql->ldap_linkid);
+	}
+	
+	// Save the attributes - Default home directory
+	if($defaulthomedir && !pql_set_domain_value($_pql->ldap_linkid, $domain, 'baseHomeDir', $defaulthomedir)) {
+		$msg = PQL_DOMAIN_DEFAULT_CHANGE_FAILED . ":&nbsp;" . ldap_error($_pql->ldap_linkid);
+	}
+	
+	// Save the attributes - Default mail directory
+	if($defaultmaildir && !pql_set_domain_value($_pql->ldap_linkid, $domain, 'baseMailDir', $defaultmaildir)) {
+		$msg = PQL_DOMAIN_DEFAULT_CHANGE_FAILED . ":&nbsp;" . ldap_error($_pql->ldap_linkid);
+	}
+	
+	// Save the attributes - Default quota
+	if($defaultquota && !pql_set_domain_value($_pql->ldap_linkid, $domain, 'defaultQuota', $defaultquota)) {
+		$msg = PQL_DOMAIN_DEFAULT_CHANGE_FAILED . ":&nbsp;" . ldap_error($_pql->ldap_linkid);
+	}
+	
+	// The creator is by default the administrator
+	if(! pql_set_domain_value($_pql->ldap_linkid, $domain, 'administrator', $USER_DN)) {
+		$msg = PQL_DOMAIN_DEFAULT_CHANGE_FAILED . ":&nbsp;" . ldap_error($_pql->ldap_linkid);
+	}
+	
+	// redirect to domain-details
+	if($msg == "") {
+		$msg = urlencode(pql_complete_constant(PQL_DOMAIN_ADD_OK, array("domain" => $domain)));
+	}
+	header("Location: " . PQL_URI . "domain_detail.php?domain=$domain&msg=$msg&rlnb=1");
 } else {
-    $msg = urlencode(PQL_DOMAIN_ADD_FAILED . ":&nbsp;" . ldap_error($_pql->ldap_linkid));
-    header("Location: " . PQL_URI . "home.php?msg=$msg");
+	$msg = urlencode(PQL_DOMAIN_ADD_FAILED . ":&nbsp;" . ldap_error($_pql->ldap_linkid));
+	header("Location: " . PQL_URI . "home.php?msg=$msg");
 }
 ?>
 
