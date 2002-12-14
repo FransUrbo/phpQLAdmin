d19fa4e1d2a14bcf6238abf70e424a8702e673d1
turbo <turbo>
2002-12-14 20:47:35 +0000

    * Replaced '<?php echo ...; ?>' with shorter '<?=...?>'. * Filter information depending on wether ADVANCED mode have been choosen   (checkbox in the left frame controls this). Session variable... * Retabbing...

--- domain_detail.php
+++ domain_detail.php
@@ -32,11 +32,7 @@ if(isset($rlnb) and PQL_AUTO_RELOAD) {
   </script>
 <?php
 }
-?>
-  <span class="title1"><?php echo $domain ?></span>
 
-  <br><br>
-<?php
 $_pql = new pql($USER_DN, $USER_PASS);
 
 // check if domain exist
@@ -47,6 +43,10 @@ if(!pql_domain_exist($_pql->ldap_linkid, PQL_LDAP_BASEDN, $domain)){
 
 // Get default domain name for this domain
 $defaultdomain = pql_get_domain_value($_pql->ldap_linkid, $domain, "defaultdomain");
+?>
+  <span class="title1">Domain: <?=$domain?><?php if($defaultdomain) {echo " ($defaultdomain)";} ?></span>
+  <br><br>
+<?php
 $basehomedir   = pql_get_domain_value($_pql->ldap_linkid, $domain, "basehomedir");
 $basemaildir   = pql_get_domain_value($_pql->ldap_linkid, $domain, "basemaildir");
 $basequota     = pql_get_domain_value($_pql->ldap_linkid, $domain, "basequota");
@@ -56,51 +56,50 @@ $defaultdomain_link = "<a href=\"domain_edit_attributes.php?attrib=defaultdomain
 $basehomedir_link   = "<a href=\"domain_edit_attributes.php?attrib=basehomedir&domain=$domain\"><img src=\"images/edit.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"Modify default homedirectory for $domain\"></a>";
 $basemaildir_link   = "<a href=\"domain_edit_attributes.php?attrib=basemaildir&domain=$domain\"><img src=\"images/edit.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"Modify default maildirectory for $domain\"></a>";
 $basequota_link     = "<a href=\"domain_edit_attributes.php?attrib=basequota&domain=$domain\"><img src=\"images/edit.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"Modify default quota for $domain\"></a>";
+	if($ADVANCED_MODE == 1) {
 ?>
-
   <table cellspacing="0" cellpadding="3" border="0">
-    <th colspan="3" align="left"><?php echo PQL_DOMAIN_DEFAULT_VALUES ?></th>
+    <th colspan="3" align="left"><?=PQL_DOMAIN_DEFAULT_VALUES?></th>
       <tr class="<?php table_bgcolor(); ?>">
-        <td class="title"><?php echo PQL_DOMAIN_DEFAULT_NAME; ?></td>
+        <td class="title"><?=PQL_DOMAIN_DEFAULT_NAME?></td>
         <td><?php if($defaultdomain != $domain){echo $defaultdomain;}else{echo PQL_UNSET;} ?></td>
-        <td><?php echo $defaultdomain_link; ?></td>
+        <td><?=$defaultdomain_link?></td>
       </tr>
   
       <tr class="<?php table_bgcolor(); ?>">
-        <td class="title"><?php echo PQL_DOMAIN_DEFAULT_HOMEDIR; ?></td>
+        <td class="title"><?=PQL_DOMAIN_DEFAULT_HOMEDIR?></td>
         <td><?php if($basehomedir != $domain){echo $basehomedir;}else{echo PQL_UNSET;} ?></td>
-        <td><?php echo $basehomedir_link; ?></td>
+        <td><?=$basehomedir_link?></td>
       </tr>
   
       <tr class="<?php table_bgcolor(); ?>">
-        <td class="title"><?php echo PQL_DOMAIN_DEFAULT_MAILDIR; ?></td>
+        <td class="title"><?=PQL_DOMAIN_DEFAULT_MAILDIR?></td>
         <td><?php if($basemaildir != $domain){echo $basemaildir;}else{echo PQL_UNSET;} ?></td>
-        <td><?php echo $basemaildir_link; ?></td>
+        <td><?=$basemaildir_link?></td>
       </tr>
   
       <tr class="<?php table_bgcolor(); ?>">
-        <td class="title"><?php echo PQL_LDAP_MAILQUOTA_TITLE; ?></td>
+        <td class="title"><?=PQL_LDAP_MAILQUOTA_TITLE?></td>
         <td><?php if($basequota != $domain){echo $basequota;}else{echo PQL_UNSET;} ?></td>
-        <td><?php echo $basequota_link; ?></td>
+        <td><?=$basequota_link?></td>
       </tr>
     </th>
   </table>
-  
   <br><br>
-
 <?php
+}
 $users = pql_get_user($_pql->ldap_linkid, PQL_LDAP_BASEDN, $domain);
 ?>
   <table cellspacing="0" cellpadding="3" border="0">
-    <th colspan="3" align="left"><?php echo PQL_USER_REGISTRED ?></th>
+    <th colspan="3" align="left"><?=PQL_USER_REGISTRED?></th>
 <?php
 if(is_array($users)){
 ?>
       <tr>
-        <td class="title"><?php echo PQL_USER ?></td>
-        <td class="title"><?php echo PQL_USER_ID ?></td>
-        <td class="title"><?php echo PQL_LDAP_ACCOUNTSTATUS_STATUS ?></td>
-        <td class="title"><?php echo PQL_OPTIONS ?></td>
+        <td class="title"><?=PQL_USER?></td>
+        <td class="title"><?=PQL_USER_ID?></td>
+        <td class="title"><?=PQL_LDAP_ACCOUNTSTATUS_STATUS?></td>
+        <td class="title"><?=PQL_OPTIONS?></td>
       </tr>
 
 <?php
@@ -115,10 +114,10 @@ if(is_array($users)){
 		$status = pql_ldap_accountstatus($status[0]);
 ?>
       <tr class="<?php table_bgcolor(); ?>">
-        <td><a href="user_detail.php?domain=<?php echo $domain ?>&user=<?php echo urlencode($user)?>"><?php echo $cn; ?></a></td>
-        <td><?php echo $uid; ?></td>
-        <td><?php echo $status; ?></td>
-        <td><a href="user_detail.php?domain=<?php echo $domain ?>&user=<?php echo urlencode($user)?>"><img src="images/edit.png" width="12" height="12" alt="<?php echo PQL_USER_EDIT ?>" border="0"></a>&nbsp;&nbsp;<a href="user_del.php?domain=<?php echo $domain;?>&user=<?php echo urlencode($user); ?>"><img src="images/del.png" width="12" height="12" alt="<?php echo PQL_USER_DELETE ?>" border="0"></a></td>
+        <td><a href="user_detail.php?domain=<?=$domain?>&user=<?=urlencode($user)?>"><?=$cn?></a></td>
+        <td><?=$uid?></td>
+        <td><?=$status?></td>
+        <td><a href="user_detail.php?domain=<?=$domain?>&user=<?=urlencode($user)?>"><img src="images/edit.png" width="12" height="12" alt="<?=PQL_USER_EDIT?>" border="0"></a>&nbsp;&nbsp;<a href="user_del.php?domain=<?=$domain?>&user=<?=urlencode($user)?>"><img src="images/del.png" width="12" height="12" alt="<?=PQL_USER_DELETE?>" border="0"></a></td>
       </tr>
 
 <?php
@@ -127,14 +126,14 @@ if(is_array($users)){
 	// no users registred
 ?>
       <tr class="<?php table_bgcolor(); ?>">
-        <td colspan="4"><?php echo PQL_USER_NONE ?></td>
+        <td colspan="4"><?=PQL_USER_NONE?></td>
       </tr>
 <?php
 }
 ?>
 
       <tr class="subtitle">
-        <td colspan="4"><a href="user_add.php?domain=<?php echo $domain; ?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"> <?php echo PQL_USER_NEW; ?></a></td>
+        <td colspan="4"><a href="user_add.php?domain=<?=$domain?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"> <?=PQL_USER_NEW?></a></td>
       </tr>
     </th>
   </table>
@@ -145,33 +144,33 @@ if(is_array($users)){
   <br><br>
   
   <table cellspacing="0" cellpadding="3" border="0">
-    <th colspan="2" align="left"><?php echo PQL_DOMAIN_CHANGE_ATTRIBUTE_TITLE; ?></th>
+    <th colspan="2" align="left"><?=PQL_DOMAIN_CHANGE_ATTRIBUTE_TITLE?></th>
       <tr>
-        <td class="title"><?php echo PQL_DOMAIN_CHANGE_ATTRIBUTE; ?></td>
-        <td class="title"><?php echo PQL_OPTIONS; ?></td>
+        <td class="title"><?=PQL_DOMAIN_CHANGE_ATTRIBUTE?></td>
+        <td class="title"><?=PQL_OPTIONS?></td>
       </tr>
   
       <tr class="<?php table_bgcolor(); ?>">
-        <td><?php echo PQL_LDAP_ACCOUNTSTATUS_STATUS ?></td>
-        <td><a href="domain_edit_attributes.php?attrib=accountstatus&domain=<?php echo $domain; ?>&set=active"><?php echo PQL_LDAP_ACCOUNTSTATUS_CHANGE_ACTIVE; ?></a>
-  	| <a href="domain_edit_attributes.php?attrib=accountstatus&domain=<?php echo $domain; ?>&set=nopop"><?php echo PQL_LDAP_ACCOUNTSTATUS_CHANGE_NOPOP; ?></a>
-  	| <a href="domain_edit_attributes.php?attrib=accountstatus&domain=<?php echo $domain; ?>&set=disabled"><?php echo PQL_LDAP_ACCOUNTSTATUS_CHANGE_DISABLE; ?></a>
+        <td><?=PQL_LDAP_ACCOUNTSTATUS_STATUS?></td>
+        <td><a href="domain_edit_attributes.php?attrib=accountstatus&domain=<?=$domain?>&set=active"><?=PQL_LDAP_ACCOUNTSTATUS_CHANGE_ACTIVE?></a>
+  	| <a href="domain_edit_attributes.php?attrib=accountstatus&domain=<?=$domain?>&set=nopop"><?=PQL_LDAP_ACCOUNTSTATUS_CHANGE_NOPOP?></a>
+  	| <a href="domain_edit_attributes.php?attrib=accountstatus&domain=<?=$domain?>&set=disabled"><?=PQL_LDAP_ACCOUNTSTATUS_CHANGE_DISABLE?></a>
         </td>
       </tr>
   
       <tr class="<?php table_bgcolor(); ?>">
-        <td><?php echo PQL_LDAP_MAILQUOTA_TITLE ?></td>
-        <td><a href="domain_edit_attributes.php?attrib=mailquota&domain=<?php echo $domain; ?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
+        <td><?=PQL_LDAP_MAILQUOTA_TITLE?></td>
+        <td><a href="domain_edit_attributes.php?attrib=mailquota&domain=<?=$domain?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
       </tr>
   
       <tr class="<?php table_bgcolor(); ?>">
-        <td><?php echo PQL_LDAP_MAILHOST_TITLE ?></td>
-        <td><a href="domain_edit_attributes.php?attrib=mailhost&domain=<?php echo $domain; ?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
+        <td><?=PQL_LDAP_MAILHOST_TITLE?></td>
+        <td><a href="domain_edit_attributes.php?attrib=mailhost&domain=<?=$domain?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
       </tr>
   
       <tr class="<?php table_bgcolor(); ?>">
-        <td><?php echo PQL_LDAP_DELIVERYMODE_TITLE ?></td>
-        <td><a href="domain_edit_attributes.php?attrib=deliverymode&domain=<?php echo $domain; ?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
+        <td><?=PQL_LDAP_DELIVERYMODE_TITLE?></td>
+        <td><a href="domain_edit_attributes.php?attrib=deliverymode&domain=<?=$domain?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
       </tr>
     </th>
   </table>
@@ -184,93 +183,97 @@ if(is_array($users)){
 <?php
 // dns informations
 $res = getmxrr($defaultdomain, $rec, $weight);
+if($ADVANCED_MODE == 1) {
 ?>
   <table cellspacing="0" cellpadding="3" border="0">
-    <th colspan="3" align="left"><?php echo PQL_DNS_TITLE ?></th>
+    <th colspan="3" align="left"><?=PQL_DNS_TITLE?></th>
 <?php
-if(count($rec) == 0){
+	if(count($rec) == 0){
 ?>
       <tr class="<?php table_bgcolor(); ?>">
-        <td colspan="2"><?php echo PQL_DNS_NONE ?></td>
+        <td colspan="2"><?=PQL_DNS_NONE?></td>
       </tr>
 <?php
-} else {
+	} else {
 ?>
       <tr>
-        <td class="title"><?php echo PQL_DNS_MAILHOST ?></td>
-        <td class="title"><?php echo PQL_DNS_PRIORITY ?></td>
+        <td class="title"><?=PQL_DNS_MAILHOST?></td>
+        <td class="title"><?=PQL_DNS_PRIORITY?></td>
       </tr>
 <?php
-	asort($weight);
-	foreach($weight as $key => $prio){
+		asort($weight);
+		foreach($weight as $key => $prio){
 ?>
       <tr class="<?php table_bgcolor(); ?>">
-        <td><?php echo $rec[$key]; ?></td>
-        <td align="right"><?php echo $prio; ?></td>
+        <td><?=$rec[$key]?></td>
+        <td align="right"><?=$prio?></td>
       </tr>
 <?php
-	} // end foreach
-} // end if (count($rec) == 0)
+		} // end foreach
+	} // end if (count($rec) == 0)
 ?>
     </th>
   </table>
-  
+
   <br><br>
 
 <?php
-if(PQL_LDAP_CONTROL_USE){
-    if(pql_control_search_attribute($_pql_control->ldap_linkid, PQL_LDAP_CONTROL_BASEDN, "locals", $domain)){
-		$locals = PQL_YES;
-		if(!PQL_LDAP_CONTROL_AUTOADDLOCALS){
-			$locals_link = "<a href=\"control_edit_attribute.php?attrib=locals&type=del&set=$domain&submit=1\"><img src=\"images/del.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"remove $domain from locals\"></a>";
+	if(PQL_LDAP_CONTROL_USE){
+		if(pql_control_search_attribute($_pql_control->ldap_linkid, PQL_LDAP_CONTROL_BASEDN, "locals", $domain)){
+			$locals = PQL_YES;
+			if(!PQL_LDAP_CONTROL_AUTOADDLOCALS){
+				$locals_link = "<a href=\"control_edit_attribute.php?attrib=locals&type=del&set=$domain&submit=1\"><img src=\"images/del.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"remove $domain from locals\"></a>";
+			} else {
+				$locals_link = "&nbsp;";
+			}
 		} else {
-			$locals_link = "&nbsp;";
+			$locals = PQL_NO;
+			if(!PQL_LDAP_CONTROL_AUTOADDLOCALS){
+				$locals_link = "<a href=\"control_edit_attribute.php?attrib=locals&type=add&set=$domain&submit=1\"><img src=\"images/edit.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"add $domain to locals\"></a>";
+			} else {
+				$locals_link = "&nbsp;";
+			}
 		}
-    } else {
-		$locals = PQL_NO;
-		if(!PQL_LDAP_CONTROL_AUTOADDLOCALS){
-			$locals_link = "<a href=\"control_edit_attribute.php?attrib=locals&type=add&set=$domain&submit=1\"><img src=\"images/edit.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"add $domain to locals\"></a>";
+		
+		if(pql_control_search_attribute($_pql_control->ldap_linkid, PQL_LDAP_CONTROL_BASEDN, "rcpthosts", $domain)){
+			$rcpthosts = PQL_YES;
+			$rcpthosts_link = "<a href=\"control_edit_attribute.php?attrib=rcpthosts&type=del&set=$domain&submit=1\"><img src=\"images/del.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"remove $domain from rcpthosts\"></a>";
 		} else {
-			$locals_link = "&nbsp;";
+			$rcpthosts = PQL_NO;
+			$rcpthosts_link = "<a href=\"control_edit_attribute.php?attrib=rcpthosts&type=add&set=$domain&submit=1\"><img src=\"images/edit.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"add $domain to rcpthosts\"></a>";
 		}
-    }
-	
-    if(pql_control_search_attribute($_pql_control->ldap_linkid, PQL_LDAP_CONTROL_BASEDN, "rcpthosts", $domain)){
-	$rcpthosts = PQL_YES;
-	$rcpthosts_link = "<a href=\"control_edit_attribute.php?attrib=rcpthosts&type=del&set=$domain&submit=1\"><img src=\"images/del.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"remove $domain from rcpthosts\"></a>";
-    } else {
-	$rcpthosts = PQL_NO;
-	$rcpthosts_link = "<a href=\"control_edit_attribute.php?attrib=rcpthosts&type=add&set=$domain&submit=1\"><img src=\"images/edit.png\" width=\"12\" height=\"12\" border=\"0\" alt=\"add $domain to rcpthosts\"></a>";
-    }
 ?>
   <table cellspacing="0" cellpadding="3" border="0">
-    <th align="left"><?php echo PQL_OPTIONS ?></th>
+    <th align="left"><?=PQL_OPTIONS?></th>
       <tr class="<?php table_bgcolor(); ?>">
         <td class="title">Defined in locals</td>
-        <td><?php echo $locals; ?></td>
-        <td><?php echo $locals_link; ?></td>
+        <td><?=$locals?></td>
+        <td><?=$locals_link?></td>
       </tr>
   
       <tr class="<?php table_bgcolor(); ?>">
         <td class="title">Defined in rcpthosts</td>
-        <td><?php echo $rcpthosts; ?></td>
-        <td><?php echo $rcpthosts_link; ?></td>
+        <td><?=$rcpthosts?></td>
+        <td><?=$rcpthosts_link?></td>
+      </tr>
+    </th>
+  </table>
+
+  <br><br>
+
+<?php
+	} // end if(PQL_LDAP_CONTROL_USE) ...
+?>
+  <table cellspacing="0" cellpadding="3" border="0">
+    <th align="left"><?=PQL_ACTIONS?></th>
+      <tr class="<?php table_bgcolor(); ?>">
+        <td><a href="domain_del.php?domain=<?=$domain?>"><?=PQL_DOMAIN_DEL?></a></td>
       </tr>
     </th>
   </table>
 <?php
-} // end if(PQL_LDAP_CONTROL_USE) ...
+} // end if ADVANCE mode
 ?>
-
-  <br><br>
-  
-  <table cellspacing="0" cellpadding="3" border="0">
-    <th align="left"><?php echo PQL_ACTIONS; ?></th>
-      <tr class="<?php table_bgcolor(); ?>">
-        <td><a href="domain_del.php?domain=<?php echo $domain; ?>"><?php echo PQL_DOMAIN_DEL;?></a></td>
-      </tr>
-    </th>
-  </table>
 </body>
 </html>
 
