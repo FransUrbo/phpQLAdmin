688b5aebd95fee406b54301f2ebdee7476ef7d23
turbo <turbo>
2002-12-18 16:46:44 +0000

    Similar to domain_detail.php. Should give a short summary of control information. Not done though..

---
+++ control_detail.php
@@ -0,0 +1,17 @@
+<?php
+// Show details on QmailLDAP/Control host
+// $Id: control_detail.php,v 1.1 2002-12-18 16:46:44 turbo Exp $
+session_start();
+?>
+<html>
+</html>
+<?php
+/*
+ * Local variables:
+ * mode: php
+ * mode: font-lock
+ * tab-width: 4
+ * End:
+ */
+?>
+
