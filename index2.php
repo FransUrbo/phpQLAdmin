95ffddbd7c1bb3db5682e8adaf6970e35b24e94b
turbo <turbo>
2002-12-18 16:12:56 +0000

    Target index2 when changing checkbox 'Advanced mode' - make sure it reloads all frames.

--- index2.php
+++ index2.php
@@ -7,10 +7,32 @@ require("config.inc");
     <title><?php echo PQL_WHOAREWE;?></title>
   </head>
 
-  <frameset cols="250,*" rows="*" border="0" frameborder="0"> 
-    <frame src="left.php" name="pqlnav">
-    <frame src="home.php" name="pqlmain">
+<?php
+if(isset($advanced)) {
+    // Advance mode - show controls and mailinglist managers
+?>
+  <frameset cols="250,*" rows="*" border="1" frameborder="0"> 
+    <frameset cols="*" rows="60%,*" border="1" frameborder="0">
+      <frame src="left.php?advanced=1" name="pqlnav">
+      <frameset cols="*" rows="50%,*" border="1" frameborder="0">
+        <frame src="left-control.php" name="pqlnavctrl">
+        <frame src="left-ezmlm.php" name="pqlnavezmlm">
+      </frameset>
+    </frameset>
+    <frame src="home.php?advanced=1" name="pqlmain">
   </frameset>
+<?php
+} else {
+    // Not running in advanced mode, don't show the
+    // controls/mailinglist managers
+?>
+  <frameset cols="250,*" rows="*" border="0" frameborder="0"> 
+    <frame src="left.php?advanced=0" name="pqlnav">
+    <frame src="home.php?advanced=0" name="pqlmain">
+  </frameset>
+<?php
+}
+?>
 
   <noframes>
     <body bgcolor="#FFFFFF">
