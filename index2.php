c0e42f98361d89804a56f5e3e1b2ef7716a8eadb
turbo <turbo>
2002-12-20 01:50:19 +0000

    * Changed include path in apache/php4: base dir of phpQLAdmin - 'require(include/'. * Check define PQL_LDAP_EZMLM_USE if we should show the ezmlm mailing list   manager at al (we might not be running on the list host - don't show the frame).

--- index2.php
+++ index2.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require("config.inc");
+require("include/config.inc");
 ?>
 <html>
   <head>
@@ -14,10 +14,20 @@ if(isset($advanced)) {
   <frameset cols="250,*" rows="*" border="1" frameborder="0"> 
     <frameset cols="*" rows="60%,*" border="1" frameborder="0">
       <frame src="left.php?advanced=1" name="pqlnav">
+<?php
+	if(PQL_LDAP_EZMLM_USE) {
+?>
       <frameset cols="*" rows="50%,*" border="1" frameborder="0">
         <frame src="left-control.php" name="pqlnavctrl">
         <frame src="left-ezmlm.php" name="pqlnavezmlm">
       </frameset>
+<?php
+	} else {
+?>
+      <frame src="left-control.php" name="pqlnavctrl">
+<?php
+	}
+?>
     </frameset>
     <frame src="home.php?advanced=1" name="pqlmain">
   </frameset>
