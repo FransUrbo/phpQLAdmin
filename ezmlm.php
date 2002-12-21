50d87ff9241640bd8bd9e1684ce4e1f7c8b0bcb4
turbo <turbo>
2002-12-21 10:37:59 +0000

    Remove all logic, it's in ezmlm-detail.php now.

--- ezmlm.php
+++ ezmlm.php
@@ -1,46 +1,16 @@
 <?php
-// $Id: ezmlm.php,v 1.1 2002-12-20 01:56:56 turbo Exp $
+// $Id: ezmlm.php,v 1.2 2002-12-21 10:37:59 turbo Exp $
 //
-session_start();
 
-require("include/pql.inc");
 include("header.html");
-
-// Ezmlm mailing list manager class(es) and depends
-// http://www.phpclasses.org/browse.html/package/177
-require("ezmlmmgr/library/forms.php");
-require("ezmlmmgr/library/common/tableclass.php");
-require("ezmlmmgr/library/links.php");
-require("ezmlmmgr/library/ezmlm/editezmlmlistclass.php");
-
-$ezmlm = new edit_ezmlm_list_class();
-
-// Enable some needed functions.
-$ezmlm->delete = $ezmlm->editing_texts = $ezmlm->editing = 1;
-$ezmlm->deleting = $ezmlm->adding = 1;
-
-// TODO: Hardcoded defaults to get it looking like SOMETHING...
-require("ezmlmmgr/locale/ezmlm/editezmlmlistclass-en.php");
-$ezmlm->ezmlm_user = "alias";
-$ezmlm->ezmlm_user_home = "/var/lists";
-$ezmlm->rowsperpage = 100;
-$ezmlm->values = array("idiom" => 'en');
-$ezmlm->preferred_idiom = 'en';
-$ezmlm->centerframes=0;
-
-if(!($ezmlm->load())) {
-    $error = $ezmlm->error;
-}
-?>
-  <br><span class="title1">Mailinglists</span><br>
-<?php
-if(!strcmp($error,"")) {
-    echo $ezmlm->outputlist();
-} else {
-    echo "Error: ";
-    echo $error;
-}
 ?>  
+  <span class="title1">Ezmlm Mailinglist Control</span>
+
+  <br><br>
+Here you can manage your ezmlm mailinglists. Currently all lists have to be located in /var/lists, but
+that will change eventually (ie, when I get around to finish it up).
+  <br><br>
+
   </body>
 </html>
 
