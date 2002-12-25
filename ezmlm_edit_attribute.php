f2e7a79f3e200541576ef4ffb4cfdc7411af2acb
turbo <turbo>
2002-12-25 01:18:04 +0000

    * Send path to binary, ml username and basedir for lists when initializing the class. * Missed yet more ./ in require()/include() statements. * Implemented editing (toggle) of variables, addition/removal of subscribers   and change of list owner.   TODO: Change from and listparent.

--- ezmlm_edit_attribute.php
+++ ezmlm_edit_attribute.php
@@ -1,26 +1,82 @@
 <?php
-// $Id: ezmlm_edit_attribute.php,v 1.3 2002-12-23 11:46:26 turbo Exp $
+// $Id: ezmlm_edit_attribute.php,v 1.4 2002-12-25 01:18:04 turbo Exp $
 //
 session_start();
 
-require("include/pql.inc");
-require("include/pql_ezmlm.inc");
+require("./include/pql.inc");
+require("./include/pql_ezmlm.inc");
 
 // Initialize
-$ezmlm = new ezmlm();
-require("ezmlm-hardcoded.php");
+$ezmlm = new ezmlm('/usr/bin', 'alias', '/var/lists');
 
-include("header.html");
+include("./header.html");
 
 // Load list of mailinglists
 if($ezmlm->readlists()) {
-	if($ezmlm->mailing_lists[$listno][$attrib]) {
-		$list = $ezmlm->mailing_lists[$listno]["name"] . "@$domain";
+	if($ezmlm->mailing_lists[$listno]["name"]) {
+		$list   = $ezmlm->mailing_lists[$listno]["name"];
+		$domain = $ezmlm->mailing_lists[$listno]["host"];
+	}	
 
-		// TODO
-		echo "<b>MODIFY</b> attribute <u>$attrib</u> on list <u>$list</u>.<br>Current value: <b>" . $ezmlm->mailing_lists[$listno][$attrib] . "</b><br>";
+	// TODO: Same for 'listparent' and 'fromaddress' when/if we need it...
+	if(($attrib == 'subscriber') or ($attrib == 'owner')) {
+		if(($submit != 'save') and !$value) {
+			if($attrib == 'subscriber') {
+				$title1 = 'Add email address to subscription list';
+				$title2 = 'Subscription address';
+			} elseif($attrib == 'owner') {
+				$title1 = 'Edit list owner address';
+				$title2 = 'List owner address';
+			}
+
+			// Haven't submitted yet, output the questionaire
+?>
+  <span class="title1"><?=$title1?></span>
+
+  <br><br>
+
+  <form action="<?=$PHP_SELF?>" method="post">
+	<table cellspacing="0" cellpadding="3" border="0">
+      <th colspan="3" align="left"><?=$title2?></th>
+        <tr class="<?php table_bgcolor(); ?>">
+          <td class="title"><?=PQL_EMAIL?></td>
+<?php
+			if($attrib == 'subscriber') {
+?>
+          <td><input type="text" name="subscriber" value="<?=$subscriber?>"></td>
+<?php
+			} elseif($attrib == 'owner') {
+?>
+          <td><input type="text" name="owner" value="<?=$owner?>"></td>
+<?php
+			}
+?>
+        </tr>
+      </th>
+    </table>
+
+    <input type="hidden" name="listno" value="<?=$listno?>">
+    <input type="hidden" name="domain" value="<?=$domain?>">
+    <input type="hidden" name="attrib" value="<?=$attrib?>">
+    <input type="submit" name="submit" value="save">
+  </form>
+<?php
+		} else {
+			// Save the value of list owner
+
+			if($attrib == 'subscriber') {
+				if(! $value) {
+					$ezmlm->subscribe($list, $subscriber);
+				} else {
+					$ezmlm->unsubscribe($list, $value);
+				}
+			} elseif($attrib == 'owner') {
+				$ezmlm->updatelistentry(0, $list, $domain, $attrib, $owner);
+			}
+		}
 	} else {
-		echo "Attribute $attrib on list number $listno <b>not defined</b><br>";
+		// Toggle configuration value
+		$ezmlm->updatelistentry(0, $list, $domain, $attrib, $ezmlm->mailing_lists[$listno]);
 	}
 }
 
@@ -32,3 +88,5 @@ if($ezmlm->readlists()) {
  * End:
  */
 ?>
+  </body>
+</html>
