a0b15045ef6b9f738e7ff15954e4e0b2f94e0052
turbo <turbo>
2002-12-26 01:08:01 +0000

    Get basemaildir for domain which list we should delete, call ezmlm->deletelistentry() to delete it.

--- ezmlm_del.php
+++ ezmlm_del.php
@@ -1,24 +1,32 @@
 <?php
-// $Id: ezmlm_del.php,v 1.6 2002-12-25 11:30:43 turbo Exp $
+// $Id: ezmlm_del.php,v 1.7 2002-12-26 01:08:01 turbo Exp $
 //
 session_start();
 
 require("./include/pql.inc");
 require("./include/pql_ezmlm.inc");
 
-// Initialize
-$ezmlm = new ezmlm('alias', '/var/lists');
-
 include("./header.html");
 
+// Initialize
+$_pql = new pql($USER_HOST_USR, $USER_DN, $USER_PASS, false, 0);
+
+// Get base directory for mails
+if(!($path = pql_get_domain_value($_pql->ldap_linkid, $domain, "basemaildir"))) {
+	// TODO: What if we can't find the base maildir path!?
+	die("Can't get baseMailDir path from domain '$domain'!");
+}
+
 // Load list of mailinglists
-if($ezmlm->readlists()) {
+if($ezmlm = new ezmlm('alias', $path)) {
 	if(is_array($ezmlm->mailing_lists[$listno])) {
-		$list = $ezmlm->mailing_lists[$listno]["name"] . "@" . $ezmlm->mailing_lists[$listno]["host"];
+		$listname = $ezmlm->mailing_lists[$listno]["name"] . "@" . $ezmlm->mailing_lists[$listno]["host"];
+		$listpath = $ezmlm->mailing_lists[$listno]["directory"];
 
 		// TODO
-		echo "<b>DELETE</b> list <u>$list</u>.<br>";
-	} echo {
+		echo "<b>DELETE</b> list <u>$listname</u> ($listpath).<br>";
+		$ezmlm->deletelistentry($listno);
+	} else {
 		echo "List does not exists<br>";
 	}
 }
