d735adffc5fbea8de1420b572280621194c5268e
turbo <turbo>
2003-01-07 09:12:59 +0000

    Updated information that lists need not be in /var/lists.

--- ezmlm.php
+++ ezmlm.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: ezmlm.php,v 1.3 2002-12-24 19:08:58 turbo Exp $
+// $Id: ezmlm.php,v 1.4 2003-01-07 09:12:59 turbo Exp $
 //
 
 include("./header.html");
@@ -7,9 +7,7 @@ include("./header.html");
   <span class="title1">Ezmlm Mailinglist Control</span>
 
   <br><br>
-Here you can manage your ezmlm mailinglists. Currently all lists have to be located in /var/lists, but
-that will change eventually (ie, when I get around to finish it up).
-  <br><br>
+Here you can manage your ezmlm mailinglists. Mailing lists have to be located under each domain's mail directory (baseMailDir in the domain object) and is organized per domain name in the list frame.<br>
 
   </body>
 </html>
