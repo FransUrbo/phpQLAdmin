248b4f36b945fdee739b662b346731c59e92a27e
turbo <turbo>
2003-01-08 06:11:55 +0000

    * Show more information   - Allow change of LDAP server [y/n]   - Verify user/domain deletions [y/n]   - Objectclasses used to create users   - Domain/Users reference attribute (uid/cn, dc/ou etc) * Don't show 'LDAP control root dn' - no point, it's user logged in!

--- config_detail.php
+++ config_detail.php
@@ -36,6 +36,25 @@ if(PQL_ALLOW_ABSOLUTE_PATH){
 } else {
     $allow_absolute_path = "no";
 }
+
+if(PQL_LDAP_CHANGE_SERVER){
+    $allow_change_server = "yes";
+} else {
+    $allow_change_server = "no";
+}
+
+if(PQL_LDAP_EZMLM_USE){
+    $ezmlm_use = "yes";
+} else {
+    $ezmlm_use = "no";
+}
+
+if(PQL_VERIFY_DELETE){
+    $verify_delete = "yes";
+} else {
+    $verify_delete = "no";
+}
+
 ?>
   <span class="title1">phpQLAdmin configuration</span>
   <br><br>
@@ -44,6 +63,23 @@ if(PQL_ALLOW_ABSOLUTE_PATH){
 <tr>
         <td class="title">Language</td>
         <td class="<?php table_bgcolor(); ?>"><?php echo PQL_LANG; ?>&nbsp;</td>
+</tr>
+
+<tr>
+	<td class="title">Hostmaster</td>
+	<td class="<?php table_bgcolor(); ?>"><?php echo PQL_HOSTMASTER; ?>&nbsp;</td>
+</tr>
+
+<tr></tr>
+
+<tr>
+	<td class="title">Manage Controls DB</td>
+	<td class="<?php table_bgcolor(); ?>"><?php echo $control_use; ?>&nbsp;</td>
+</tr>
+
+<tr>
+	<td class="title">Manage EZMLM mailinglists</td>
+	<td class="<?php table_bgcolor(); ?>"><?=$ezmlm_use?>&nbsp;</td>
 </tr>
 
 <tr>
@@ -57,26 +93,26 @@ if(PQL_ALLOW_ABSOLUTE_PATH){
 </tr>
 
 <tr>
-	<td class="title">use control db</td>
-	<td class="<?php table_bgcolor(); ?>"><?php echo $control_use; ?>&nbsp;</td>
-</tr>
-
-<tr>
 	<td class="title">Automatically replicate domains to locals</td>
 	<td class="<?php table_bgcolor(); ?>"><?php echo $control_autoaddlocals; ?>&nbsp;</td>
 </tr>
 
 <tr>
-	<td class="title">allow absolute mailbox paths</td>
+	<td class="title">Allow absolute mailbox paths</td>
 	<td class="<?php table_bgcolor(); ?>"><?php echo $allow_absolute_path; ?>&nbsp;</td>
 </tr>
 
-<tr></tr>
+<tr>
+	<td class="title">Allow change of LDAP server</td>
+	<td class="<?php table_bgcolor(); ?>"><?=$allow_change_server?>&nbsp;</td>
+</tr>
 
 <tr>
-	<td class="title">Hostmaster</td>
-	<td class="<?php table_bgcolor(); ?>"><?php echo PQL_HOSTMASTER; ?>&nbsp;</td>
+	<td class="title">Verify user/domain deletions etc <b>[recomended!]</b></td>
+	<td class="<?php table_bgcolor(); ?>"><?=$verify_delete?>&nbsp;</td>
 </tr>
+
+<tr></tr>
 
 <tr>
 	<td class="title">LDAP host</td>
@@ -98,10 +134,6 @@ if(PQL_ALLOW_ABSOLUTE_PATH){
 	<td class="<?php table_bgcolor(); ?>"><?=$USER_SEARCH_DN_CTR?>&nbsp;</td>
 </tr>
 <tr>
-	<td class="title">LDAP control root dn</td>
-	<td class="<?php table_bgcolor(); ?>"><?=$USER_DN?>&nbsp;</td>
-</tr>
-<tr>
 	<td class="title">Password encryption schemes</td>
 	<td class="<?php table_bgcolor(); ?>"><?php echo PQL_PASSWORD_SCHEMES; ?>&nbsp;</td>
 </tr>
@@ -117,6 +149,36 @@ if(PQL_ALLOW_ABSOLUTE_PATH){
 	}
 ?>
 
+<tr></tr>
+
+<tr>
+	<td class="title">User objectclasses</td>
+	<td class="<?php table_bgcolor(); ?>"><?php echo PQL_LDAP_OBJECTCLASS_USERID; ?>&nbsp;</td>
+</tr>
+<?php
+$objectclasses = split(" ", PQL_LDAP_OBJECTCLASS_USER_EXTRA);
+foreach($objectclasses as $oc) {
+?>
+<tr>
+	<td class="title"></td>
+	<td class="<?php table_bgcolor(); ?>"><?=$oc?>&nbsp;</td>
+</tr>
+<?php
+}
+?>
+
+<tr></tr>
+
+<tr>
+	<td class="title">Reference users with</td>
+	<td class="<?php table_bgcolor(); ?>"><?=PQL_LDAP_REFERENCE_USERS_WITH?></td>
+</tr>
+
+<tr>
+	<td class="title">Reference domains with</td>
+	<td class="<?php table_bgcolor(); ?>"><?=PQL_LDAP_ATTR_DOMAIN?> (<?=PQL_LDAP_OBJECTCLASS_DOMAIN?>)</td>
+</tr>
+	
 <tr class="subtitle">
 	<td colspan="2"><img src="images/info.png" width="16" height="16" border="0">the phpQLAdmin configuration values are stored in config.inc&nbsp;</td>
 </tr>
