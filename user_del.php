c300930059b2a7cdbce5152d563fb0a64073b444
turbo <turbo>
2003-01-10 18:10:07 +0000

    'else output are you sure' in wrong place\!

--- user_del.php
+++ user_del.php
@@ -12,35 +12,35 @@ include("./header.html");
   <br><br>
 <?php
 	if(isset($ok) || PQL_VERIFY_DELETE){
-	$delete_forwards = (isset($delete_forwards) || PQL_VERIFY_DELETE) ? true : false;
-  	$_pql = new pql($USER_HOST_USR, $USER_DN, $USER_PASS);
-
-	// delete the user
-	if(pql_remove_user($_pql->ldap_linkid, $USER_SEARCH_DN_USR, $domain, $user, $delete_forwards)){
-		$msg = PQL_USER_DEL_OK . ": <b>" . $user . "</b>";
-		$rlnb = "&rlnb=1";
-  	} else {
-    	$msg = PQL_USER_DEL_FAILED . ":&nbsp;" . ldap_error($_pql->ldap_linkid);
-    }
-
-  	// redirect to domain-detail page
-	$msg = urlencode($msg);
-	header("Location: " . PQL_URI . "domain_detail.php?domain=$domain&msg=$msg$rlnb");
+		$delete_forwards = (isset($delete_forwards) || PQL_VERIFY_DELETE) ? true : false;
+		$_pql = new pql($USER_HOST_USR, $USER_DN, $USER_PASS);
+		
+		// delete the user
+		if(pql_remove_user($_pql->ldap_linkid, $USER_SEARCH_DN_USR, $domain, $user, $delete_forwards)){
+			$msg = PQL_USER_DEL_OK . ": <b>" . $user . "</b>";
+			$rlnb = "&rlnb=1";
+		} else {
+			$msg = PQL_USER_DEL_FAILED . ":&nbsp;" . ldap_error($_pql->ldap_linkid);
+		}
+		
+		// redirect to domain-detail page
+		$msg = urlencode($msg);
+		header("Location: " . PQL_URI . "domain_detail.php?domain=$domain&msg=$msg$rlnb");
+	} else {
+		echo PQL_SURE;
 ?>
 <br>
-<form action="<?php echo $PHP_SELF; ?>" method="GET">
-	<input type="hidden" name="user" value="<?php echo $user; ?>">
-	<input type="hidden" name="domain" value="<?php echo $domain; ?>">
-	
-	<input type="checkbox" name="delete_forwards" checked> <?php echo PQL_USER_DELETE_FORWARDS; ?><br><br>
-	<input type="submit" name="ok" value="<?php echo PQL_YES; ?>">
-	<input type="button" name="back" value="<?php echo PQL_NO; ?>" onClick="history.back();">
-</form>
+  <form action="<?php echo $PHP_SELF; ?>" method="GET">
+    <input type="hidden" name="user" value="<?php echo $user; ?>">
+    <input type="hidden" name="domain" value="<?php echo $domain; ?>">
+        
+    <input type="checkbox" name="delete_forwards" checked> <?php echo PQL_USER_DELETE_FORWARDS; ?><br><br>
+    <input type="submit" name="ok" value="<?php echo PQL_YES; ?>">
+    <input type="button" name="back" value="<?php echo PQL_NO; ?>" onClick="history.back();">
+  </form>
 <br>
 <?php
-  } else {
-	echo PQL_SURE;
-  } // end of if
+	} // end of if
 ?>
 </body>
 </html>
