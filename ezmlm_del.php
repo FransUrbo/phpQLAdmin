06e891e2e5d25c3f43cb8e1f6bc69caf112b79b1
turbo <turbo>
2003-01-14 07:21:40 +0000

    * Enable redirection to list detail page (list_forward() function). * Removed a TODO comment.

--- ezmlm_del.php
+++ ezmlm_del.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: ezmlm_del.php,v 1.7 2002-12-26 01:08:01 turbo Exp $
+// $Id: ezmlm_del.php,v 1.8 2003-01-14 07:21:40 turbo Exp $
 //
 session_start();
 
@@ -10,6 +10,15 @@ include("./header.html");
 
 // Initialize
 $_pql = new pql($USER_HOST_USR, $USER_DN, $USER_PASS, false, 0);
+
+// forward back to list detail page
+function list_forward($domainname, $msg){
+	global $domain;
+
+    $msg = urlencode($msg);
+    $url = "home.php?msg=$msg&rlnb=3";
+    header("Location: " . PQL_URI . "$url");
+}
 
 // Get base directory for mails
 if(!($path = pql_get_domain_value($_pql->ldap_linkid, $domain, "basemaildir"))) {
@@ -23,8 +32,6 @@ if($ezmlm = new ezmlm('alias', $path)) {
 		$listname = $ezmlm->mailing_lists[$listno]["name"] . "@" . $ezmlm->mailing_lists[$listno]["host"];
 		$listpath = $ezmlm->mailing_lists[$listno]["directory"];
 
-		// TODO
-		echo "<b>DELETE</b> list <u>$listname</u> ($listpath).<br>";
 		$ezmlm->deletelistentry($listno);
 	} else {
 		echo "List does not exists<br>";
