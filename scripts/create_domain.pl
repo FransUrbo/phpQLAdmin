9e20bd0bc09df8a06b7ecf102a2a8820f6b64f57
turbo <turbo>
2003-01-16 15:04:40 +0000

    Create the homedirectory if defined.

--- scripts/create_domain.pl
+++ scripts/create_domain.pl
@@ -36,6 +36,23 @@ if($ENV{"PQL_MAILDIRS"} && $ENV{"PQL_WEBUSER"}) {
     chown($ENV{"PQL_WEBUSER"}, 0, $DIR);
 }
 
+if($ENV{"PQL_HOMEDIRS"}) {
+    $DIR = $ENV{PQL_HOMEDIRS};
+    @dirs = split('/', $DIR);
+
+    $directory = '/' . $dirs[1] . '/';
+    for($i=1; $dirs[$i]; $i++) {
+	if(! -d $directory) {
+	    print "Creating $directory\n";
+	    if(! mkdir($directory) ) {
+		print "Unsuccessfull in creating $dir, $!\n";
+	    }
+	}
+
+	$directory .= $dirs[$i+1] . '/'
+    }
+}
+
 #print "// beg ($0)\n";
 #system('/usr/bin/env');
 #print "// end\n";
