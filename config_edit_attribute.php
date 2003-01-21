5aebe9b38cd7d0ea49287017a8af49d415ed8d82
turbo <turbo>
2003-01-21 13:32:38 +0000

    * Replace reference to 'include/pql.inc' to 'include/pql_config.inc'.   The latter later includes the former, and manages the configuration   retreival etc. * Make sure we reload the nav frame if successfull in changing attribute

--- config_edit_attribute.php
+++ config_edit_attribute.php
@@ -1,22 +1,24 @@
 <?php
 // Edit and set configuration values in the LDAP database
-// $Id: config_edit_attribute.php,v 1.2 2003-01-20 06:24:14 turbo Exp $
+// $Id: config_edit_attribute.php,v 1.3 2003-01-21 13:32:38 turbo Exp $
 //
 session_start();
 
-require("./include/pql.inc");
+require("./include/pql_config.inc");
 $_pql = new pql($USER_HOST, $USER_DN, $USER_PASS);
 
-// forward back to configuration detail page
-function attribute_forward($msg) {
-    $msg = urlencode($msg);
-    $url = "config_detail.php?msg=$msg";
-    header("Location: " . PQL_URI . "$url");
-}
-
-require("./include/pql_config.inc");
 include("./include/attrib.config.inc");
 include("./header.html");
+
+// forward back to configuration detail page
+function attribute_forward($msg, $rlnb = false) {
+    $msg = urlencode($msg);
+    $url = "config_detail.php?msg=$msg";
+	if($rlnb)
+	  $url .= "&rlnb=1";
+
+    header("Location: " . PQL_URI . "$url");
+}
 ?>
   <span class="title1">Change configuration value</span>
 
