6cfeda558b226b9c5d0f0ae6bcb329ccb6af56ea
turbo <turbo>
2003-01-31 13:21:46 +0000

    Dissalow the ezmlm frame if ordinary user.

--- index2.php
+++ index2.php
@@ -13,7 +13,7 @@ if(isset($advanced)) {
 ?>
   <frameset cols="250,*" rows="*" border="1" frameborder="0">
 <?php
-	if(PQL_LDAP_EZMLM_USE or (PQL_LDAP_CONTROL_USE and $ALLOW_CONTROL_CREATE)) {
+	if((!$SINGLE_USER and PQL_LDAP_EZMLM_USE) or (PQL_LDAP_CONTROL_USE and $ALLOW_CONTROL_CREATE)) {
 ?>
     <frameset cols="*" rows="60%,*" border="1" frameborder="0">
 <?php
@@ -21,7 +21,7 @@ if(isset($advanced)) {
 ?>
       <frame src="left.php?advanced=1" name="pqlnav">
 <?php
-	if(PQL_LDAP_EZMLM_USE and PQL_LDAP_CONTROL_USE and $ALLOW_CONTROL_CREATE) {
+	if((!$SINGLE_USER and PQL_LDAP_EZMLM_USE) and PQL_LDAP_CONTROL_USE and $ALLOW_CONTROL_CREATE) {
 ?>
       <frameset cols="*" rows="50%,*" border="1" frameborder="0">
 <?php
@@ -33,12 +33,12 @@ if(isset($advanced)) {
 <?php
 	}
 
-	if(PQL_LDAP_EZMLM_USE) {
+	if(!$SINGLE_USER and PQL_LDAP_EZMLM_USE) {
 ?>
         <frame src="left-ezmlm.php" name="pqlnavezmlm">
 <?php
 	}
-	if(PQL_LDAP_EZMLM_USE and PQL_LDAP_CONTROL_USE and $ALLOW_CONTROL_CREATE) {
+	if(!$SINGLE_USER and PQL_LDAP_EZMLM_USE and PQL_LDAP_CONTROL_USE and $ALLOW_CONTROL_CREATE) {
 ?>
       </frameset>
 <?php
@@ -49,7 +49,7 @@ if(isset($advanced)) {
       <frame src="left-control.php" name="pqlnavctrl">
 <?php
 	}
-	if(PQL_LDAP_EZMLM_USE or (PQL_LDAP_CONTROL_USE and $ALLOW_CONTROL_CREATE)) {
+	if((!$SINGLE_USER and PQL_LDAP_EZMLM_USE) or (PQL_LDAP_CONTROL_USE and $ALLOW_CONTROL_CREATE)) {
 ?>
     </frameset>
 <?php
