2119516bc0e6572c0b42bb664bd4d658141dcfa9
turbo <turbo>
2003-03-31 08:00:26 +0000

    Show a select box if there's more than one namingContexts, so we can specify in what db backend to create the domain in.

--- domain_add_form.php
+++ domain_add_form.php
@@ -1,11 +1,13 @@
 <?php
 // Input form page to create a domain branch in database
-// $Id: domain_add_form.php,v 1.4 2003-01-21 13:39:40 turbo Exp $
+// $Id: domain_add_form.php,v 1.5 2003-03-31 08:00:26 turbo Exp $
 //
 session_start();
 require("./include/pql_config.inc");
 
 include("./header.html");
+
+$_pql = new pql($USER_HOST, $USER_DN, $USER_PASS);
 ?>
   <span class="title1">Create domain branch in LDAP server <?=$USER_HOST?></span>
 
@@ -14,18 +16,34 @@ include("./header.html");
   <form action="domain_add.php" method="post">
     <table cellspacing="0" cellpadding="3" border="0">
       <th colspan="3" align="left"><?=PQL_DOMAIN_ADD?></th>
+<?php
+if($_pql->ldap_basedn[1]) {
+    // We have more than one Root DN (namingContexts), show
+    // a drop down with every Root DN...
+?>
         <tr class="<?php table_bgcolor(); ?>">
-          <table>
-            <td class="title">Branch name</td>
-            <td>
-              <input type="text" name="domain" value="<?php echo $domain; ?>" size="26">
-              <input type="submit" value="<?="--&gt;&gt;"?>">
-              <table cellspacing="0" cellpadding="3" border="0">
-                <th>
-                  <tr class="<?php table_bgcolor(); ?>">
-                    <td>
-                      <img src="images/info.png" width="16" height="16" alt="" border="0"></td>
-                    <td>
+          <td class="title">Branch base</td>
+          <td>
+            <select name="rootdn">
+<?php foreach($_pql->ldap_basedn as $dn)  { ?>
+              <option value="<?=$dn?>"><?=$dn?></option>
+<?php } ?>
+            </select>
+          </td>
+        </tr>
+
+<?php } ?>
+        <tr>
+          <td class="title">Branch name</td>
+          <td>
+            <input type="text" name="domain" value="<?php echo $domain; ?>" size="26">
+            <input type="submit" value="<?="--&gt;&gt;"?>">
+            <table cellspacing="0" cellpadding="3" border="0">
+              <th>
+                <tr class="<?php table_bgcolor(); ?>">
+                  <td>
+                    <img src="images/info.png" width="16" height="16" alt="" border="0"></td>
+                  <td>
 <?php
 	    if(PQL_LDAP_REFERENCE_DOMAINS_WITH == "dc") {
 		// We're using a domain object
@@ -35,12 +53,11 @@ include("./header.html");
 		echo PQL_DOMAIN_ADD_INFO_OU;
 	    }
 ?>
-                    </td>
-                  </tr>
-                </th>
-              </table>
-            </td>
-          </table>
+                  </td>
+                </tr>
+              </th>
+            </table>
+          </td>
         </tr>
       </th>
     </table>
