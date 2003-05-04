1d74490686743bfdc2683eab17c3fb7ffbd5185a
turbo <turbo>
2003-05-04 09:24:36 +0000

    Include the rootdn when calling user_del.php - so we can find the correct config variable.

--- user_detail.php
+++ user_detail.php
@@ -593,7 +593,7 @@ if(!$SINGLE_USER) {
   <table cellspacing="0" cellpadding="3" border="0">
     <th align="left"><?=PQL_LANG_ACTIONS?></th>
       <tr class="<?php table_bgcolor(); ?>">
-        <td><a href="user_del.php?domain=<?=$domain?>&user=<?=$user?>"><?=PQL_LANG_USER_DELETE?></a></td>
+        <td><a href="user_del.php?rootdn=<?=$rootdn?>&domain=<?=$domain?>&user=<?=$user?>"><?=PQL_LANG_USER_DELETE?></a></td>
       </tr>
     </th>
   </table>
