bda81910c78b4233f2e39a36f94f69fdeedb2bd1
turbo <turbo>
2003-05-15 07:03:23 +0000

    Move all 'information tables' to separate files in ./tables/. One file, one table. That way it's easier to 'redesign' ONE table, without much clutter...

--- control_detail.php
+++ control_detail.php
@@ -1,6 +1,6 @@
 <?php
 // Show details on QmailLDAP/Control host
-// $Id: control_detail.php,v 1.15 2003-05-04 10:04:57 turbo Exp $
+// $Id: control_detail.php,v 1.16 2003-05-15 07:02:02 turbo Exp $
 session_start();
 require("./include/pql_config.inc");
 
@@ -58,245 +58,18 @@ if($config["PQL_GLOB_CONTROL_USE"]) {
 	// reload navigation frame
 	parent.frames.pqlnavctrl.location.reload();
   //--></script>
-<?php
-	}
-?>
+<?php } ?>
 
   <span class="title1">Mailserver: <?=$mxhost?></span>
 
   <br><br>
 
-  <table cellspacing="0" cellpadding="3" border="0">
-    <th colspan="3" align="left">Base values</th>
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">RDN</td>
-        <td><?=$cn?></td>
-        <td></td>
-        <td></td>
-      </tr>
-  
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">Fully qualified domain name</td>
-        <td><?=$mxhost?></td>
-        <td></td>
-        <td></td>
-      </tr>
-  
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">Default domainname</td>
-        <td><?=$defaultdomain?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=defaultdomain&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=defaultdomain&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">Plusdomain</td>
-        <td><?=$plusdomain?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=plusdomain&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=plusdomain&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">LDAP Server</td>
-        <td><?=$ldapserver?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldapserver&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldapserver&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">LDAP Search base</td>
-        <td><?=$ldapbasedn?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldapbasedn&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldapbasedn&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">LDAP login</td>
-        <td><?=$ldaplogin?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldaplogin&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldaplogin&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">LDAP password</td>
-        <td><?=$ldappassword?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldappassword&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldappassword&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">LDAP Rebind</td>
-        <td><?php if($ldaprebind) { echo "Yes"; $set=0; } else { echo "No"; $set=1; } ?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldaprebind&submit=1&type=modify&set=<?=$set?>"><img src="images/edit.png" width="12" height="12" alt="toggle value" border="0"></a></td>
-        <td></td>
-      </tr>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">Default quota</td>
-        <td><?=$ldapdefaultquota?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldapdefaultquota&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldapdefaultquota&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">Default DOT mode</td>
-        <td><?=$ldapdefaultdotmode?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldapdefaultdotmode&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=ldapdefaultdotmode&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-<?php
-	if($dirmaker) { ?>
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">Directory maker</td>
-        <td><?=$dirmaker?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=dirmaker&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=dirmaker&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-<?php
-	}
-
-	if($quotawarning) {
-?>
-      <tr class="<?php table_bgcolor(); ?>">
-        <td class="title">Default quota warning</td>
-        <td><?=$quotawarning?></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=quotawarning&type=add"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a></td>
-        <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=quotawarning&type=del"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-      </tr>
-
-<?php
-	}
-?>
-    </th>
-  </table>
-
-  <br>
-
-  <table cellspacing="0" cellpadding="0" border="0">
-    <th valign="top" align="left">Locals
-      <!-- LOCALS -->
-      <table cellspacing="0" cellpadding="3" border="0">
-        <th>
-          <tr class="<?php table_bgcolor(); ?>">
-            <td class="title">locals</td>
-<?php
-	$i = 0;
-	if(is_array($locals)) {
-		foreach($locals as $local) {
-			if(!$i) {
-?>
-            <td><?=$local?></td>
-            <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=locals&type=add&set=<?=$local?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a>&nbsp;&nbsp;<a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=locals&type=del&set=<?=$local?>&submit=1"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-          </tr>
-
-<?php
-			} else {
-?>
-          <tr class="<?php table_bgcolor(); ?>">
-            <td class="title"></td>
-            <td><?=$local?></td>
-            <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=locals&type=add&set=<?=$local?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a>&nbsp;&nbsp;<a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=locals&type=del&set=<?=$local?>&submit=1"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-          </tr>
-
-<?php
-			}
-			$i++;
-		}
-	} else {
-?>
-          <td><i>not defined</i></td>
-          <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=locals&type=add&set=<?=$local?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a>&nbsp;&nbsp;<a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=locals&type=del&set=<?=$local?>&submit=1"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-        </tr>
-<?php
-	}
-?>
-          <tr class="<?php table_bgcolor(); ?>">
-            <td class="title" align="right"><img src="images/edit.png" width="12" height="12"></td>
-            <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=locals">Add value</a></td>
-            <td></td>
-          </tr>
-
-          <tr class="<?php table_bgcolor(); ?>">
-            <td class="title" align="right"><img src="images/edit.png" width="12" height="12"></td>
-            <td><a href="control_edit_attribute.php?attrib=locals&type=replicate&mxhost=<?=$mxhost?>&submit=1">Replicate</a></td>
-            <td></td>
-          </tr>
-        </th>
-      </table>
-    </th>
-
-    <th>&nbsp;&nbsp;&nbsp;&nbsp;</th>
-
-    <th valign="top" align="left">RCPT Hosts
-      <!-- RCPTHOSTS -->
-      <table cellspacing="0" cellpadding="3" border="0">
-        <th>
-          <tr class="<?php table_bgcolor(); ?>">
-            <td class="title">rcpthosts</td>
-<?php
-	$i = 0;
-	if(is_array($rcpthosts)) {
-		foreach($rcpthosts as $rcpthost) {
-			if(!$i) {
-?>
-            <td><?=$rcpthost?></td>
-            <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=rcpthosts&type=add&set=<?=$rcpthost?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a>&nbsp;&nbsp;<a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=rcpthosts&type=del&set=<?=$rcpthost?>&submit=1"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-          </tr>
-
-<?php
-			} else {
-?>
-          <tr class="<?php table_bgcolor(); ?>">
-            <td class="title"></td>
-            <td><?=$rcpthost?></td>
-            <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=rcpthosts&type=add&set=<?=$rcpthost?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"></a>&nbsp;&nbsp;<a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=rcpthosts&type=del&set=<?=$rcpthost?>&submit=1"><img src="images/del.png" width="12" height="12" alt="" border="0"></a></td>
-          </tr>
-
-<?php
-			}
-			$i++;
-		}
-	} else {
-?>
-          <td><i>not defined</i></td>
-          <td></td>
-        </tr>
-<?php
-	}
-?>
-          <tr class="<?php table_bgcolor(); ?>">
-            <td class="title" align="right"><img src="images/edit.png" width="12" height="12"></td>
-            <td><a href="control_edit_attribute.php?mxhost=<?=$mxhost?>&attrib=rcpthosts">Add value</a></td>
-            <td></td>
-          </tr>
-        </th>
-      </table>
-    </th>
-  </table>
-
-  <br>
-
-  <table cellspacing="0" cellpadding="3" border="0">
-    <th align="left"><?=PQL_LANG_ACTIONS?></th>
-      <tr class="subtitle">
-        <td colspan="4"><a href="installmailserver.php?domain=<?=$domain?>&mxhost=<?=$mxhost?>"><img src="images/edit.png" width="12" height="12" border="0">Create mailserver install script</a></td>
-      </tr>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td><a href="control_del.php?mxhost=<?=$mxhost?>"><img src="images/edit.png" width="12" height="12" border="0">Delete mailserver control object</a></td>
-      </tr>
-    </th>
-  </table>
-<?php  
-} else {
-	// PQL_GLOB_CONTROL_USE isn't set!
-?>
+<?php include("./tables/control_details-base.inc"); ?>
+<?php include("./tables/control_details-hosts.inc"); ?>
+<?php include("./tables/control_details-action.inc"); ?>
+<?php } else { ?>
   <span class="title1">PQL_GLOB_CONTROL_USE isn't set, won't show control information</span>
-<?php
-}
-
+<?php }
 /*
  * Local variables:
  * mode: php
--- ezmlm_detail.php
+++ ezmlm_detail.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: ezmlm_detail.php,v 1.15 2003-04-04 06:53:47 turbo Exp $
+// $Id: ezmlm_detail.php,v 1.16 2003-05-15 07:02:02 turbo Exp $
 //
 session_start();
 require("./include/pql_config.inc");
@@ -25,135 +25,10 @@ if($domain) {
 	
 	if(!is_numeric($listno)) {
 		// No list, show all lists in the domain
-?>
-  <span class="title1">Domain: <?=$domain?> (domainname: <?=$domainname?>)</span>
-  <br><br>
-
-  <table cellspacing="0" cellpadding="3" border="0">
-    <th colspan="3" align="left">Existing lists</th>
-      <tr>
-        <td class="title">Address</td>
-        <td class="title">Subscribers</td>
-        <td class="title">Owner</td>
-        <td class="title">Options</td>
-      </tr>
-
-<?php
-		foreach($ezmlm->mailing_lists_hostsindex as $listdomain => $listarray) {
-			// Go through each list in the domain
-			foreach($listarray as $listname => $listno) {
-?>
-      <tr class="<?php table_bgcolor(); ?>">
-        <td><?=$listname."@".$listdomain?></td>
-        <td align="right"><?=$ezmlm->mailing_lists[$listno]["subscribers"]?></td>
-        <td><?=$ezmlm->mailing_lists[$listno]["owner"]?></td>
-        <td><a href="ezmlm_detail.php?domain=<?=$domain?>&domainname=<?=$domainname?>&listno=<?=$listno?>"><img src="images/edit.png" width="12" height="12" alt="edit list" border="0"></a>&nbsp;&nbsp;<a href="ezmlm_del.php?domain=<?=$domain?>&domainname=<?=$domainname?>&listno=<?=$listno?>"><img src="images/del.png" width="12" height="12" alt="delete list" border="0"></a>&nbsp;&nbsp;<a href="ezmlm_edit_attribute.php?domain=<?=$domain?>&domainname=<?=$domainname?>&listno=<?=$listno?>&attrib=subscriber">add subscriber</a></td>
-      </tr>
-<?php
-			}
-		}
-?>
-
-      <tr class="subtitle">
-        <td colspan="4"><a href="ezmlm_add.php?domain=<?=$domain?>&domainname=<?=$domainname?>"><img src="images/edit.png" width="12" height="12" alt="" border="0"> Register new list</a></td>
-      </tr>
-    </th>
-  </table>
-<?php
+		include("./tables/ezmlm_details-lists.inc");
 	} else {
 		// We got a list, show if's details
-?>
-  <span class="title1">List: <?=$ezmlm->mailing_lists[$listno]["name"]."@".$domainname?></span>
-  <br><br>
-
-  <table cellspacing="0" cellpadding="3" border="0">
-    <th colspan="3" align="left">List information</th>
-      <tr>
-        <td class="title">Configuration</td>
-        <td class="title">Value</td>
-        <td class="title">Options</td>
-      </tr>
-<?php
-		foreach($ezmlm->mailing_lists[$listno] as $key => $value) {
-			// Defined value
-			if(!is_array($value)) {
-				// Not an array (ie, not subscriber/text value)
-				
-				if($value == 1)
-				  $value = 'Yes';
-				elseif(!$value)
-				  $value = 'No';
-				
-				// if this is number of subscribers, don't output it
-				if(($key != 'subscribers') and ($key != 'directory') and ($key != 'dotpath')) {
-?>
-
-      <tr class="<?php table_bgcolor(); ?>">
-        <td><?=$key?></td>
-        <td><?=$value?></td>
-<?php
-				}
-			} else {
-				$j = 0;
-
-				// Only output the key value once (the first entry)
-				foreach($value as $x) {
-?>
-      <tr class="<?php table_bgcolor(); ?>">
-<?php
-					if($j == 0) {
-?>
-        <td><?=$key?></td>
-<?php
-					} else {
-?>
-        <td></td>
-<?php
-					}
-?>
-        <td><?=$x?></td>
-        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="ezmlm_edit_attribute.php?domain=<?=$domain?>&domainname=<?=$domainname?>&listno=<?=$listno?>&attrib=<?=$key?>&value=<?=$x?>"><img src="images/del.png" width="12" height="12" alt="delete attribute" border="0"></a></td>
-<?php
-					$j++;
-				}
-			}
-
-			if(($key != 'subscriber') and ($key != 'directory') and ($key != 'dotpath')) {
-				if(($key == 'name') or ($key == 'local') or ($key == 'subscribers') or ($key == 'host')) {
-					// Special circumstances - not editable/deletable
-?>
-        <td></td>
-<?php
-				} else {
-					// Editable/Deletable values
-?>
-        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="ezmlm_edit_attribute.php?domain=<?=$domain?>&domainname=<?=$domainname?>&listno=<?=$listno?>&attrib=<?=$key?>"><img src="images/edit.png" width="12" height="12" alt="toggle value" border="0"></a>&nbsp;&nbsp;<a href="ezmlm_edit_attribute.php?domain=<?=$domain?>&domainname=<?=$domainname?>&listno=<?=$listno?>&attrib=<?=$key?>"></a></td>
-<?php
-				}
-			}
-?>
-      </tr>
-<?php
-		}
-?>
-      <tr class="<?php table_bgcolor(); ?>">
-        <td></td>
-        <td><a href="ezmlm_edit_attribute.php?domain=<?=$domain?>&domainname=<?=$domainname?>&listno=<?=$listno?>&attrib=subscriber">add subscriber</a></td>
-        <td></td>
-      </tr>
-    </th>
-  </table>
-
-  <br>
-
-  <table cellspacing="0" cellpadding="3" border="0">
-    <th align="left"><?=PQL_LANG_ACTIONS?></th>
-      <tr class="<?php table_bgcolor(); ?>">
-        <td><a href="ezmlm_del.php?domain=<?=$domain?>&listno=<?=$listno?>">Delete list</a></td>
-      </tr>
-    </th>
-  </table>
-<?php
+		include("./tables/ezmlm_details-detail.inc");
 	}
 }
 
