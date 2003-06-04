9417fdb2096cae5a07a24ba14321705de3a58aef
turbo <turbo>
2003-06-04 07:20:14 +0000

    * If we can't find the image, create a blank one * More code comments added

--- tools/navbutton.php
+++ tools/navbutton.php
@@ -1,16 +1,27 @@
 <? // http://www.thescripts.com/serversidescripting/php/articles/dynamicimagesinphp3.0/page0.html
-$string=implode($argv," ");
+// Prepare the text - remove unwanted characters etc
+$string = implode($argv," ");
 $string = stripslashes($string);
-$string = ereg_replace("\r\n","\n",$string) ;
-$string = ereg_replace("%20"," ",$string) ;
+$string = ereg_replace("\r\n", "\n", $string) ;
+$string = ereg_replace("%20",  " ", $string);
 
+// Create the image
 $myimage = ImageCreateFromGif("images/unselected.gif");
-$black   = ImageColorAllocate($myimage, 0, 0, 0);
-$width   = (imagesx($myimage)-7.5*strlen($string))/2;
+if(!$myimage) {
+    // No image, create a blank one
+    $myimage = ImageCreate(255, 19);
+    $white   = ImageColorAllocate($myimage, 255, 255, 255);
+    ImageFilledRectangle($myimage, 0, 0, 255, 19, $white);
+}
 
+// Setup colors
+$black = ImageColorAllocate($myimage, 0, 0, 0);
+$width = (ImageSX($myimage)-7.5*strlen($string))/2;
+    
+// Write text to image
 ImageString($myimage, 3, $width, 3, $string, $black);
-
-# Show image
+    
+// Show image
 Header("Content-Type: image/gif");
 ImageGif($myimage);
 ImageDestroy($myimage);
