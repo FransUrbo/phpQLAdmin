48a69e0959a728269c9caf69c1c93a439bbd6177
turbo <turbo>
2003-07-15 11:11:54 +0000

    If we're "single user" (ie, non-admin), we should NOT search through the whole database, but from the users branch. - Slightly changed filter in the pql_search() function. Should not do a   'cn=*' (or uid=*) search, but use the filter that search.php sets up.

--- search.php
+++ search.php
@@ -56,14 +56,46 @@ switch($filter_type) {
     break;
 }
 
-// get userlist that matches filter
-foreach($_pql->ldap_basedn as $dn) {
-	$dn = urldecode($dn);
+if(!$GLOBALS["SINGLE_USER"]) {
+	// Admin of some sort - look in the whole database for a user
+	// that matches filter
+	foreach($_pql->ldap_basedn as $dn) {
+		$dn = urldecode($dn);
+		
+		$usrs = pql_search($_pql->ldap_linkid, $dn, $filter);
+		for($i=0; $usrs[$i]; $i++) {
+			$is_group = 0;
+			
+			// Check if this object is a (posix)Group object.
+			$ocs = pql_get_userattribute($_pql->ldap_linkid,
+										 $usrs[$i], 'objectclass');
+			for($j=0; $ocs[$j]; $j++) {
+				if(eregi('group', $ocs[$j]))
+				  $is_group = 1;
+			}
+			
+			if(!$is_group)
+			  // It's NOT a (posix)Group object, show it in the list...
+			  $users[] = $usrs[$i];
+		}
+	}
+} else {
+	// Single user - only look in the same branch as the user is located in
+	$dn = '';
 
-    $usrs = pql_search($_pql->ldap_linkid, $dn, $filter);
+	// Get branch for user
+    $dnparts = ldap_explode_dn($USER_DN, 0);
+	for($i=1; $dnparts[$i]; $i++) {
+		$dn .= $dnparts[$i];
+		if($dnparts[$i+1])
+		  $dn .= ",";
+	}
+
+	// Search for the user below this DN
+	$usrs = pql_search($_pql->ldap_linkid, $dn, $filter);
 	for($i=0; $usrs[$i]; $i++) {
 		$is_group = 0;
-
+		
 		// Check if this object is a (posix)Group object.
 		$ocs = pql_get_userattribute($_pql->ldap_linkid, $usrs[$i], 'objectclass');
 		for($j=0; $ocs[$j]; $j++) {
@@ -78,7 +110,7 @@ foreach($_pql->ldap_basedn as $dn) {
 }
 ?>
   <table cellspacing="0" cellpadding="3" border="0">
-    <th colspan="4" align="left"><?=$LANG->_('Registred users')?>: (<?php echo count($users); ?>)</th>
+    <th colspan="4" align="left"><?=$LANG->_('Registred users')?>: <?php echo count($users); ?></th>
 <?php if(is_array($users)) { ?>
       <tr>
         <td class="title"><?=$LANG->_('User')?></td>
@@ -90,17 +122,23 @@ foreach($_pql->ldap_basedn as $dn) {
 <?php
 		asort($users);
 		foreach($users as $user) {
-			$uid    = pql_get_userattribute($_pql->ldap_linkid, $user, pql_get_define("PQL_GLOB_ATTR_UID"));
+			$uid    = pql_get_userattribute($_pql->ldap_linkid, $user,
+											pql_get_define("PQL_GLOB_ATTR_UID"));
 			$uid    = $uid[0];
 			
-			$cn     = pql_get_userattribute($_pql->ldap_linkid, $user, pql_get_define("PQL_GLOB_ATTR_CN"));
+			$cn     = pql_get_userattribute($_pql->ldap_linkid, $user,
+											pql_get_define("PQL_GLOB_ATTR_CN"));
 			$cn     = $cn[0];
 			
-			$mail   = pql_get_userattribute($_pql->ldap_linkid, $user, pql_get_define("PQL_GLOB_ATTR_MAIL"));
+			$mail   = pql_get_userattribute($_pql->ldap_linkid, $user,
+											pql_get_define("PQL_GLOB_ATTR_MAIL"));
 			$mail   = $mail[0];
 			
-			$status = pql_get_userattribute($_pql->ldap_linkid, $user, pql_get_define("PQL_GLOB_ATTR_ISACTIVE"));
+			$status = pql_get_userattribute($_pql->ldap_linkid, $user,
+											pql_get_define("PQL_GLOB_ATTR_ISACTIVE"));
 			$status = pql_ldap_accountstatus($status[0]);
+
+			$rootdn = pql_get_rootdn($user);
 ?>
 
       <tr class="<?php table_bgcolor(); ?>">
