f5aa1f4865cae904570c623e0a767feca71dff2a
turbo <turbo>
2003-07-29 11:34:35 +0000

    Fixing PHP parse errors - close bug 35.

--- user_add_attribute.php
+++ user_add_attribute.php
@@ -35,7 +35,7 @@ switch($attrib) {
     $include = "attrib.mailforwardingaddress.inc";
     break;
   default:
-    die(pql_complete_constant($LANG->_('Unknown attribute %attribute% in %file%'), array('attrib' => $attrib, 'file' => __FILE__));
+    die(pql_complete_constant($LANG->_('Unknown attribute %attribute% in %file%'), array('attribute' => $attrib, 'file' => __FILE__));
 }
 
 include("./include/".$include);
