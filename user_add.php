5d7635ead2c6130e014d57b3b8e445deaf1ac81f
turbo <turbo>
2003-11-15 12:19:01 +0000

    * Rewrote large part (mostly reorganized and remove duplication).   More obvious flow. * In the comments, write 'Step x' intead of 'Page x'. That's more   obvious.

--- user_add.php
+++ user_add.php
@@ -1,12 +1,10 @@
 <?php
 // add a user
-// $Id: user_add.php,v 2.72 2003-11-14 11:55:52 turbo Exp $
+// $Id: user_add.php,v 2.73 2003-11-15 12:19:01 turbo Exp $
 //
 session_start();
 require("./include/pql_config.inc");
 require("./include/pql_control.inc");
-
-$_pql = new pql($USER_HOST, $USER_DN, $USER_PASS);
 
 // Make sure we can have a ' in branch (also affects the user DN).
 $user   = eregi_replace("\\\'", "'", $user);
@@ -44,12 +42,14 @@ $basemaildir			= pql_get_domain_value($_pql, $domain, pql_get_define("PQL_GLOB_A
 $maxusers				= pql_get_domain_value($_pql, $domain, pql_get_define("PQL_GLOB_ATTR_MAXIMUMDOMAINUSERS"));
 $additionaldomainname	= pql_get_domain_value($_pql, $domain, pql_get_define("PQL_GLOB_ATTR_ADDITIONALDOMAINNAME"));
 
-// check formdata
+// Check the input
+$error = false; $error_text = array();
+switch($page_curr) {
+  case "":
+	// ------------------------------------------------
+	// Step 1: We're just starting (choose account type)
 
-// ------------------------------------------------
-// Page 1: surname, name, email, account_type, account_status
-if($submit == "") {
-	// Before continuing, let's see how many users there can be
+	// See how many users there can be
 	if($maxusers) {
 		if(count(pql_get_user($_pql->ldap_linkid, $domain)) >= $maxusers) {
 			// We have reached the maximum amount of users.
@@ -63,278 +63,78 @@ if($submit == "") {
 			die();
 		}
 	}
+	break;
 
-    $error = false;
-    $error_text = array();
+  case "one":
+	// ------------------------------------------------
+	// Step 2a: Process name, email etc
 
-    $user = $surname . " " . $name;
-    if($error == false
-       and pql_get_define("PQL_CONF_REFERENCE_USERS_WITH", $rootdn) == pql_get_define("PQL_GLOB_ATTR_CN")
-       and pql_user_exist($_pql->ldap_linkid, $user)) {
-		$error = true;
-		$error_text["username"] = pql_complete_constant($LANG->_('User %user% already exists'), array("user" => $user));
-		$error_text["name"]		= $LANG->_('Already exists');
-		$error_text["surname"]	= $LANG->_('Already exists');
-    }
-	
-    if($email == "") {
-		$error = true;
-		$error_text["email"] = $LANG->_('Missing');
-    }
-
-    if(!check_email($email)){
-		$error = true;
-		$error_text["email"] = $LANG->_('Invalid');
-    }
-	
-    if($error_text["email"] == "" and pql_email_exists($_pql, $email)) {
-		$error = true;
-		$error_text["email"] = $LANG->_('Already exists');
-    }
-	
-    // if an error occured, set displaying form to '' (initial display)
-    if($error == true) {
-		$submit = "";
-    } else {
-		$error_text = array();
-    }
-}
-
-// ------------------------------------------------
-// Page 2: uid, password, host, quota, userhost, quota_user
-if($submit == "two") {
-	$error_text = array();
-	$error = false;
-
-	// Verify/Create email address
-	if(!$email and function_exists('user_generate_email')) {
-		$email = strtolower(user_generate_email($_pql, $surname, $name, $defaultdomain, $domain, $account_type));
-
-		// Replace spaces with underscore
-		$email = preg_replace(" ", "_", $email, -1);
-
-		// Check again. There must be a email address.
-		if(!$email) {
-			$submit = "one";
-			$error = true;
-			$error_text["email"] = $LANG->_('Missing');
-		}
-	} else {
-		// Build the COMPLETE email address
-		if(! ereg("@", $email)) {
-			if($email_domain)
-			  $email = $email . "@" . $email_domain;
-			else
-			  $email = $email . "@" . $defaultdomain;
-		}
-	}
-
-	// Verify/Create uid - But only if we're referencing users with UID...
-	if(function_exists('user_generate_uid') and
-	   pql_get_define("PQL_CONF_REFERENCE_USERS_WITH", $rootdn) == pql_get_define("PQL_GLOB_ATTR_UID")) {
-		if(!$uid) {
-			$uid = strtolower(user_generate_uid($_pql, $surname, $name, $email, $domain, $account_type));
-			
-			// Check again. There should be a user name, either from the input
-			// form OR from the user_generate_uid() function above...
-			if(!$uid) {
-				$submit = "one";
-				$error = true;
-				$error_text["uid"] = $LANG->_('Missing');
-			} else {
-				if(preg_match("/[^a-z0-9\.@%_-]/i", $uid)) {
-					$submit = "one";
-					$error = true;
-					$error_text["uid"] = $LANG->_('Invalid');
-				}
-			}
-		} else {
-			$error = true;
-			$error_text["uid"] = $LANG->_('Missing') . " (" . $LANG->_('can\'t autogenerate') . ")";
-		}
-	}
-	
-	// Check the mailHost attribute/value
-	if(is_array($userhost)) {
-		if($host != "default")
-		  if(!preg_match("/^([a-z0-9]+\.{1,1}[a-z0-9]+)+$/i",$userhost[1])) {
-			  $error_text["userhost"] = $LANG->_('Invalid');
-			  $error = true;
-		  }
-	} elseif($userhost) {
-		if(!preg_match("/^([a-z0-9]+\.{1,1}[a-z0-9]+)+$/i",$userhost)) {
-			$error_text["userhost"] = $LANG->_('Invalid');
-			$error = true;
-		}
-	} elseif($account_type != "shell") {
-		// We're saving, but we don't have a mailHost value!
-		
-		// Get the primary email address domain name
-		if(ereg("@", $email))
-		  $domainname = split('@', $email);
-		
-		// Initiate a connection to the QmailLDAP/Controls DN
-		$_pql_control = new pql_control($USER_HOST, $USER_DN, $USER_PASS);
-		
-		if($_pql_control->ldap_linkid) {
-			// Find MX (or QmailLDAP/Controls with locals=$domainname)
-			$mx = pql_get_mx($_pql_control->ldap_linkid, $domainname[1]);
-			if(!$mx[1]) {
-				// There is no MX and no QmailLDAP/Controls with this
-				// domain name in locals. Die!
-				$submit = "two";
-				
-				$error = true;
-				$error_text["userhost"] = "Sorry, I can't find any MX or any QmailLDAP/Controls object that listens to this domain - <u>".$domainname[1]."</u><br>You will have to specify one manually.";
-			} else {
-				// We got a MX or QmailLDAP/Controls object. Use it.
-				$userhost[1] = $mx[1];
-				$host = "default";
-			}
-		}
-	}
-
-	// No host
-	if(!$host or !$userhost) {
-		$error = true;
-
-		if($_pql_control->ldap_linkid) {
-			$error_text["userhost"] = $LANG->_('Missing') . " (" . $LANG->_('can\'t autogenerate') . ")";
-		} else {
-			$error_text["userhost"] = $LANG->_('Missing') . " (" . $LANG->_('not using QmailLDAP/Controls') . ")";
-		}
-	}
-
-	// Check/Create the mail directory attribute/value
-	if(!$maildirectory) {
-		if(pql_get_define("PQL_CONF_REFERENCE_USERS_WITH", $rootdn) == pql_get_define("PQL_GLOB_ATTR_UID")) {
-			$maildirectory = user_generate_mailstore($_pql, $email, $domain,
-													 array(pql_get_define("PQL_GLOB_ATTR_UID") => $uid),
-													 'user');
-		} else {
-			$maildirectory = user_generate_mailstore($_pql, $email, $domain,
-													 array(pql_get_define("PQL_GLOB_ATTR_UID") => $surname." ".$name),
-													 'user');
-		}
-
-		if($maildirectory) {
-			// Replace space(s) with underscore(s)
-			$maildirectory = preg_replace('/ /', '_', $maildirectory, -1);
-		}
-	}
-
+	// Verify surname
     if($surname == "") {
 		$error = true;
 		$error_text["surname"] = $LANG->_('Missing');
     }
 	
+	// Verify lastname
     if($name == "") {
 		$error = true;
 		$error_text["name"] = $LANG->_('Missing');
     }
-}
 
-// ------------------------------------------------
-// Page 3:
-if ($submit == "save") {
-	// Verify/Create email address
-	if(!$email and function_exists('user_generate_email')) {
-		$email = strtolower(user_generate_email($_pql, $surname, $name, $defaultdomain, $domain, $account_type));
+	if($surname and $name)
+	  $user = $surname . " " . $name;
 
-		// Check again. There must be a email address.
-		if(!$email) {
-			$submit = "one";
-			$error = true;
-			$error_text["email"] = $LANG->_('Missing');
-		}
+	// Verify username
+    if(pql_get_define("PQL_CONF_REFERENCE_USERS_WITH", $rootdn) == pql_get_define("PQL_GLOB_ATTR_CN")
+       and pql_user_exist($_pql->ldap_linkid, $user)) {
+		$error = true;
+
+		$error_text["username"] = pql_complete_constant($LANG->_('User %user% already exists'), array("user" => $user));
+    }
+	
+	// Check if the email address is supplied
+    if($email == "") {
+		$error = true;
+		$error_text["email"] = $LANG->_('Missing');
 	} else {
-		// Build the COMPLETE email address
-		if(! ereg("@", $email)) {
-			if($email_domain)
-			  $email = $email . "@" . $email_domain;
-			else
-			  $email = $email . "@" . $defaultdomain;
-		}
-	}
-
-	// Verify/Create uid - But only if we're referencing users with UID...
-	if(function_exists('user_generate_uid') and
-	   pql_get_define("PQL_CONF_REFERENCE_USERS_WITH", $rootdn) == pql_get_define("PQL_GLOB_ATTR_UID")) {
-		if(!$uid) {
-			$uid = strtolower(user_generate_uid($_pql, $surname, $name, $email, $domain, $account_type));
+		// It's not supplied, generate one email address
+		if(!$email and function_exists('user_generate_email')) {
+			$email = strtolower(user_generate_email($_pql, $surname, $name, $defaultdomain, $domain, $account_type));
 			
-			// Check again. There should be a user name, either from the input
-			// form OR from the user_generate_uid() function above...
-			if(!$uid) {
-				$submit = "one";
+			// Replace spaces with underscore
+			$email = preg_replace(" ", "_", $email, -1);
+			
+			// Check again. There must be a email address.
+			if(!$email) {
+				$page_next = "one";
+				
 				$error = true;
-				$error_text["uid"] = $LANG->_('Missing');
-			} else {
-				if(preg_match("/[^a-z0-9\.@%_-]/i", $uid)) {
-					$submit = "one";
-					$error = true;
-					$error_text["uid"] = $LANG->_('Invalid');
-				}
+				$error_text["email"] = $LANG->_('Not supplied and could not auto generate');
 			}
-		} else {
-			$error = true;
-			$error_text["uid"] = $LANG->_('Missing') . " (" . $LANG->_('can\'t autogenerate') .")";
-		}
-		
-		if($error_text["uid"] == "" and pql_search_attribute($_pql->ldap_linkid, $domain,
-															 pql_get_define("PQL_GLOB_ATTR_UID"),
-															 $uid)) {
-			$error = true;
-			$error_text["uid"] = $LANG->_('Already exists');
 		}
 	}
 
-	if($account_type == "normal" or $account_type == "system" or $account_type == "shell") {
-		if($password == "") {
-			$error = true;
-			$error_text["password"] = $LANG->_('Missing');
-		}
-		
-		if(eregi("KERBEROS", $pwscheme)) {
-			// Should be in the form: userid@DOMAIN.LTD
-			// TODO: Is this regexp correct!?
-			if(! preg_match("/^[a-zA-Z0-9]+[\._-a-z0-9]*[a-zA-Z0-9]+@[A-Z0-9][-A-Z0-9]+(\.[-A-Z0-9]+)+$/", $password)) {
-				$error = true;
-				$error_text["password"] = $LANG->_('Invalid');
-			}
-		} elseif(!$crypted) {
-			// A password in cleartext, NOT already encrypted
-			if(preg_match("/[^a-z0-9]/i", $password)) {
-				$error = true;
-				$error_text["password"] = $LANG->_('Invalid');
-			}
-		}
-	} elseif($account_type == "forward") {
-		if(!check_email($forwardingaddress)) {
-			$error = true;
-			$error_text["forwardingaddress"] = $LANG->_('Invalid');
-		}
-		
-		if($forwardingaddress == "") {
-			$error = true;
-			$error_text["forwardingaddress"] = $LANG->_('Missing');
-		}
+	// First test if email is valid - must contain an '@'.
+	if(! ereg("@", $email)) {
+		// Build the COMPLETE email address
+		if($email_domain)
+		  $email = $email . "@" . $email_domain;
+		else
+		  $email = $email . "@" . $defaultdomain;
 	}
-		
-	if(($error == true) and !$ADVANCED_MODE and !$submit)
-	  $submit = "two";
-}
+	
+	// Second test if email is valid
+	if(!check_email($email)) {
+		$error = true;
+		$error_text["email"] = $LANG->_('Invalid');
+	}
 
-if($pwscheme) {
-	if(! eregi('\{', $pwscheme))
-	  $pwscheme = '{'.$pwscheme;
+	// It exists, it's valid. Does it already exists?
+	if($error_text["email"] == "" and pql_email_exists($_pql, $email)) {
+		$error = true;
+		$error_text["email"] = $LANG->_('Already exists');
+	}
 
-	if(! eregi('\}', $pwscheme))
-	  $pwscheme .= '}';
-}
-
-include("./header.html");
-?>
   <span class="title1">
     <?php echo pql_complete_constant($LANG->_('Create user in domain %domain%'), array("domain" => $orgname)); ?>
 <?php
@@ -354,31 +154,29 @@ if($ADVANCED_MODE && $account_type) {
   <br><br>
 
 <?php
-// select form to display
-switch($submit) {
-	case "":
-	  // First page (Account properties - type of account)
-	  include("./tables/user_add-properties.inc");
-      break;
-
-    // ---------------------------------- NEXT PAGE: 2
-    case "one":
-	  // Second page (User details - name, password etc)
-	  include("./tables/user_add-details.inc");
-	  break;
-
-    // ---------------------------------- NEXT PAGE: 3
-	case "two":
-	  // Third page
-	  include("./tables/user_add-additional.inc");
-	  break;
-
-    // ---------------------------------- NEXT PAGE: 4
-	case "save":
-	  // code for saving the user
-	  include("./tables/user_add-save.inc");
-	  break;
-} // end of switch($submit)
+// ------------------------------------------------
+// Select next form to display
+switch($page_next) {
+  case "":
+	// Step 1 - Choose account properties (type of account)
+	include("./tables/user_add-properties.inc");
+	break;
+	
+  case "one":
+	// Step 2 - Choose user details (name, password etc)
+	include("./tables/user_add-details.inc");
+	break;
+	
+  case "two":
+	// Step 3 - Choose additional information (mailhost, maildir etc)
+	include("./tables/user_add-additional.inc");
+	break;
+	
+  case "save":
+	// Step 4 - Save the user into DB
+	include("./tables/user_add-save.inc");
+	break;
+}
 
 /*
  * Local variables:
