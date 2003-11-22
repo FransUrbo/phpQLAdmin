aaa429706feede20da5197ce4993f2442b134543
turbo <turbo>
2003-11-22 16:09:19 +0000

    Simplify the creation of the tree view - moved to separate functions. Opera and non-opera, as well as 'top' and 'link' funcs. Should be much more generic and most of all - higher code redability!

--- left.php
+++ left.php
@@ -1,6 +1,6 @@
 <?php
 // navigation bar
-// $Id: left.php,v 2.63 2003-11-19 19:31:34 turbo Exp $
+// $Id: left.php,v 2.64 2003-11-22 16:09:09 turbo Exp $
 //
 session_start();
 
@@ -49,60 +49,55 @@ if($advanced == 1) {
   <br>
 
   <form method=post action="index2.php" target="_top">
-    <input type="checkbox" name="advanced" accesskey="a" onChange="this.form.submit()"<?=$checked?>><?=$LANG->_('\uA\Udvanced mode')?>
+    <input type="checkbox" name="advanced" accesskey="a" onChange="this.form.submit()"<?=$checked?>><?=$LANG->_('\uA\Udvanced mode')."\n"?>
   </form>
+
 <?php if($ALLOW_BRANCH_CREATE and $ADVANCED_MODE) { ?>
 
   <div id="el2Parent" class="parent">
     <nobr><a href="domain_add_form.php?rootdn=<?=$_pql->ldap_basedn[0]?>"><?php echo pql_complete_constant($LANG->_('Add %what%'), array('what' => $LANG->_('domain branch'))); ?></a></nobr>
   </div>
-<?php  } ?>
 
-  <!-- HOME -->
-<?php if($opera) { ?>
-  <div id="el2Parent" class="parent" onclick="showhide(el1Spn, el1Img)">
-    <img name="imEx" src="images/minus.png" border="0" alt="-" width="9" height="9" id="el1Img">
-    <a href="home.php"><font color="black" class="heada"><b><?=$LANG->_('Home')?></b></font></a>
-  </div>
-<?php } else { ?>
-  <div id="el1Parent" class="parent">
-    <a class="item" href="home.php" onClick="if (capable) {expandBase('el1', true); return false;}">
-      <img name="imEx" src="images/plus.png" border="0" alt="+" width="9" height="9" id="el1Img">
-    </a>
-
-    <a class="item" href="home.php">
-      <font color="black" class="heada"><b><?=$LANG->_('Home')?></b></font>
-    </a>
-  </div>
 <?php } ?>
 
-<?php if($opera) { ?>
-  <span id="el1Spn" style="display:''">
-<?php } else { ?>
-  <div id="el1Child" class="child">
-<?php } ?>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="user_search.php"><?=$LANG->_('Find user')?></a></nobr><br>
-<?php if($ADVANCED_MODE) { ?>
-<?php	if($ALLOW_BRANCH_CREATE) { ?>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="config_detail.php"><?=$LANG->_('Show configuration')?></a></nobr><br>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="config_ldaptest.php"><?=$LANG->_('Test LDAP-Connection')?></a></nobr><br>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="config_ldap.php"><?=$LANG->_('LDAP server configuration')?></a></nobr><br>
-<?php	} ?>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="doc/index.php"><?=$LANG->_('Documentation')?></a></nobr><br>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="TODO"><?=$LANG->_('What\'s left todo')?></a></nobr><br>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="CHANGES"><?=$LANG->_('What\'s been done')?></a></nobr><br>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="update_translations.php" target="_new"><?=$LANG->_('Language translator')?></a></nobr><br>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://phpqladmin.bayour.com/">phpqladmin.bayour.com</a></nobr><br>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://apache.bayour.com/anthill/" target="_new"><?=$LANG->_('Bugtracker')?></a></nobr><br>
-<?php } ?>
-<?php if($opera) { ?>
-  </span>
-<?php } else { ?>
-  </div>
-<?php } ?>
-  <!-- HOME -->
 
+  <!-- Home branch -->
 <?php
+// ----------------
+// Level 1: phpQLAdmin configuration etc
+$div_counter = 3; // Initialize the global counter
+pql_format_tree("<b>".$LANG->_('Home')."</b>", 'home.php');
+
+// Level 2a: Search links
+$links = array('user_search.php' => $LANG->_('Find user'));
+pql_format_tree($LANG->_('Search'), 0, $links, 1);
+
+if($ADVANCED_MODE) {
+    if($ALLOW_BRANCH_CREATE) {
+	// Level 2b: Configuration and tests
+	$links = array('config_detail.php'		=> $LANG->_('Show configuration'),
+		       'config_ldaptest.php'		=> $LANG->_('Test LDAP-Connection'),
+		       'config_ldap.php'		=> $LANG->_('LDAP server configuration'));
+	pql_format_tree($LANG->_('Configuration'), 0, $links, 1);
+    }
+
+    // Level 2c: Documentation etc
+    $links = array('doc/index.php'			=> $LANG->_('Documentation'),
+		   'TODO'				=> $LANG->_('What\'s left todo'),
+		   'CHANGES'				=> $LANG->_('What\'s been done'),
+		   'update_translations.php'		=> $LANG->_('Language translator'));
+    pql_format_tree($LANG->_('Documentation'), 0, $links, 1);
+
+    // Level 2d: Main site and general phpQLAdmin links
+    $links = array('http://phpqladmin.bayour.com/'	=> 'phpqladmin.bayour.com',
+		   'http://apache.bayour.com/anthill/'	=> $LANG->_('Bugtracker'));
+    pql_format_tree($LANG->_('phpQLAdmin'), 0, $links, 1);
+}
+
+// This an ending for the initial parent (level 0)
+pql_format_tree_end();
+
+// ----------------
 if($ALLOW_BRANCH_CREATE) {
     // This is a 'super-admin'. Should be able to read EVERYTHING!
     $domains = pql_domain_get($_pql);
@@ -144,6 +139,7 @@ if(!isset($domains)) {
 	    break;
 	}
     }
+
 ?>
   <!-- start domain parent -->
   <a href="user_detail.php?rootdn=<?=$rootdn?>&domain=<?=$domain?>&user=<?php echo urlencode($USER_DN); ?>"><img src="images/mail_small.png" border="0" alt="<?=$cn?>"></a>&nbsp;
@@ -153,7 +149,11 @@ if(!isset($domains)) {
 <?php
 } else {
     $SINGLE_USER = 0; session_register("SINGLE_USER");
+?>
 
+
+  <!-- Domain branches -->
+<?php
     asort($domains);
     foreach($domains as $key => $domain) {
 	// Get domain part from the DN (Example: 'dc=test,dc=net' => 'test').
@@ -165,129 +165,90 @@ if(!isset($domains)) {
 	// Get Root DN
 	$rootdn = pql_get_rootdn($domain);
 
-	$j = $key + 2;
-?>
-  <!-- start domain parent -->
-<?php if($opera) { ?>
-  <div id="el<?=$j?>Parent" class="parent" onclick="showhide(el<?=$j?>Spn, el<?=$j?>Img)">
-    <img name="imEx" src="images/minus.png" border="0" alt="-" width="9" height="9" id="el<?=$j?>Img">
-    <a href="domain_detail.php?rootdn=<?=$rootdn?>&domain=<?=$domain?>"><font color="black" class="heada"><?=$d?></font></a>
-  </div>
-<?php } else { ?>
-  <div id="el<?=$j?>Parent" class="parent">
-    <a class="item" href="domain_detail.php?rootdn=<?=$rootdn?>&domain=<?=$domain?>" onClick="if (capable) {expandBase('el<?=$j?>', true); return false;}">
-      <img name="imEx" src="images/plus.png" border="0" alt="+" width="9" height="9" id="el<?=$j?>Img">
-    </a>
+	// iterate trough all users
+	if(pql_get_define("PQL_CONF_SHOW_USERS", $rootdn)) {
+	    // Zero out the variables, othervise we won't get users in
+	    // specified domain, but also in the PREVIOUS domain shown!
+	    $users = ""; $cns = ""; unset($links);
+	    
+	    // Get all users (their DN) in this domain
+	    $users = pql_user_get($_pql->ldap_linkid, $domain);
+	    if(!is_array($users)) {
+		// No user available in this domain
 
-    <a class="item" href="domain_detail.php?rootdn=<?=$rootdn?>&domain=<?=$domain?>">
-      <font color="black" class="heada"><?=$d?></font>
-    </a>
-  </div>
-<?php } ?>
-  <!-- end domain parent -->
+		// Level 2: The users
+		$links = array("user_add.php?rootdn=$rootdn&domain=$domain" => $LANG->_('No users defined'));
+	    } else {
+		// We have users in this domain
 
-  <!-- start domain children -->
-<?php if($opera) { ?>
-  <span id="el<?=$j?>Spn" style="display:''">
-<?php } else { ?>
-  <div id="el<?=$j?>Child" class="child">
-<?php } ?>
-<?php
-      // iterate trough all users
-      if(pql_get_define("PQL_CONF_SHOW_USERS", $rootdn)) {
-	  // Zero out the variables, othervise we won't get users in
-	  // specified domain, but also in the PREVIOUS domain shown!
-	  $users = ""; $cns = "";
+		// Level 2: The users
+		$links = array("user_add.php?rootdn=$rootdn&domain=$domain" =>
+			       pql_complete_constant($LANG->_('Add %what%'),
+						     array('what' => $LANG->_('user'))));
 
-	  // Get all users (their DN) in this domain
-	  $users = pql_user_get($_pql->ldap_linkid, $domain);
-	  if(!is_array($users)) {
-	      // no user available for this domain
-?>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;
-      <img src="images/mail_small.png" border="0" alt="<?=$LANG->_('No users defined')?>">&nbsp;
-      <a href="user_add.php?rootdn=<?=$rootdn?>&domain=<?=$domain?>"><?=$LANG->_('No users defined')?></a>
-    </nobr>
+		// From the user DN, get the CN.
+		foreach ($users as $dn) {
+		    unset($cn); unset($sn); unset($gecos);
+		    
+		    $cn = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_GIVENNAME"));
+		    $sn = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_SN"));
+		    if($cn[0] && $sn[0]) {
+			// We have a givenName (first name) and a surName (last name) - combine the two
+			if($sn[0] != '_') 
+			  $cns[$dn] = $sn[0].", ".$cn[0];
+			else
+			  $cns[$dn] = $cn[0];
+		    } else {
+			// Probably don't have a givenName - get the commonName
+			$cn = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_CN"));
+			if($cn[0]) {
+			    // We have a commonName - split it up into two parts (which should be first and last name)
+			    $cn = split(" ", $cn[0]);
+			    if(!$cn[1])
+			      // Don't have second part (last name) of the commonName - MUST be a system 'user'.
+			      $cns[$dn] = "System - ".$cn[0];
+			    else {
+				// We have two parts - combine into 'Lastname, Firstname'
+				$cns[$dn] = $cn[1].", ".$cn[0];
+			    }
+			} else {
+			    // No givenName, surName or commonName - last try, get the gecos
+			    $gecos = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_GECOS"));
+			    if($gecos[0])
+			      // We have a gecos - use that as is
+			      $cns[$dn] = $gecos[0];
+//			    else
+//			      // No gecos either. Now what!?
+			}
+		    }
+		}
+		asort($cns);
+		
+		foreach($cns as $dn => $cn) {
+		    $uid = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_UID"));
+		    $uid = $uid[0];
+		    
+		    $uidnr = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_QMAILUID"));
+		    $uidnr = $uidnr[0];
+		    
+		    if(($uid != 'root') or ($uidnr != '0')) {
+			// Do NOT show root user(s) here! This should (for safty's sake)
+			// not be availible to administrate through phpQLAdmin!
+			$new = array("user_detail.php?rootdn=$rootdn&domain=$domain&user=".urlencode($dn) => $cn);
 
-    <br>
+			// Add the link to the main array
+			$links = $links + $new;
+		    }
+		}
+	    }
+	}
 
-<?php	  } else { ?>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;
-      <a href="user_add.php?rootdn=<?=$rootdn?>&domain=<?=$domain?>"><?php echo pql_complete_constant($LANG->_('Add %what%'), array('what' => $LANG->_('user'))); ?></a>
-    </nobr>
+	// Level 1: The domain name with it's users
+	$url = "domain_detail.php?rootdn=$rootdn&domain=$domain";
+	pql_format_tree($d, $url, $links, 0);
 
-    <br>
-
-<?php         // From the user DN, get the CN.
-	      foreach ($users as $dn) {
-		  unset($cn); unset($sn); unset($gecos);
-
-		  $cn = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_GIVENNAME"));
-		  $sn = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_SN"));
-		  if($cn[0] && $sn[0]) {
-		      // We have a givenName (first name) and a surName (last name) - combine the two
-		      if($sn[0] != '_') 
-			$cns[$dn] = $sn[0].", ".$cn[0];
-		      else
-			$cns[$dn] = $cn[0];
-		  } else {
-		      // Probably don't have a givenName - get the commonName
-		      $cn = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_CN"));
-		      if($cn[0]) {
-			  // We have a commonName - split it up into two parts (which should be first and last name)
-			  $cn = split(" ", $cn[0]);
-			  if(!$cn[1])
-			    // Don't have second part (last name) of the commonName - MUST be a system 'user'.
-			    $cns[$dn] = "System - ".$cn[0];
-			  else {
-			      // We have two parts - combine into 'Lastname, Firstname'
-			      $cns[$dn] = $cn[1].", ".$cn[0];
-			  }
-		      } else {
-			  // No givenName, surName or commonName - last try, get the gecos
-			  $gecos = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_GECOS"));
-			  if($gecos[0])
-			    // We have a gecos - use that as is
-			    $cns[$dn] = $gecos[0];
-//			  else
-//			    // No gecos either. Now what!?
-		      }
-		  }
-	      }
-              asort($cns);
-
-	      foreach($cns as $dn => $cn) {
-		  $uid = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_UID"));
-		  $uid = $uid[0];
-
-		  $uidnr = pql_get_attribute($_pql->ldap_linkid, $dn, pql_get_define("PQL_GLOB_ATTR_QMAILUID"));
-		  $uidnr = $uidnr[0];
-
-		  if(($uid != 'root') or ($uidnr != '0')) {
-		      // Do NOT show root user(s) here! This should (for safty's sake)
-		      // not be availible to administrate through phpQLAdmin!
-?>
-    <nobr>&nbsp;&nbsp;&nbsp;&nbsp;
-      <a href="user_detail.php?rootdn=<?=$rootdn?>&domain=<?=$domain?>&user=<?php echo urlencode($dn); ?>"><img src="images/mail_small.png" border="0" alt="<?=$cn?>"></a>&nbsp;
-      <a class="item" href="user_detail.php?rootdn=<?=$rootdn?>&domain=<?=$domain?>&user=<?php echo urlencode($dn); ?>"><?=$cn?></a>
-    </nobr>
-
-    <br>
-
-<?php
-		  }
-              }
-          }
-      }
-?>
-<?php if($opera) { ?>
-  </span>
-<?php } else { ?>
-  </div>
-<?php } ?>
-  <!-- end domain children -->
-
-<?php
+	// This an ending for the initial parent (level 0)
+	pql_format_tree_end();
     } // end foreach ($domains)
 } // end if(is_array($domains))
 
