8bf0aec59860a1ca89045ba6a9de8a3e06067567
turbo <turbo>
2004-03-14 07:42:40 +0000

    De-globalizing...

--- unit_add.php
+++ unit_add.php
@@ -1,38 +1,43 @@
 <?php
 // add a domain
-// $Id: unit_add.php,v 2.16 2004-03-11 18:13:32 turbo Exp $
+// $Id: unit_add.php,v 2.17 2004-03-14 07:42:40 turbo Exp $
 //
 session_start();
 require("./include/pql_config.inc");
 require("./include/pql_control.inc");
 
+$url["domain"] = pql_format_urls($_REQUEST["domain"]);
+$url["rootdn"] = pql_format_urls($_REQUEST["rootdn"]);
+$url["unit"]   = pql_format_urls($_REQUEST["unit"]);
+
 include("./header.html");
 ?>
-  <span class="title1"><?php echo pql_complete_constant($LANG->_('Create subbranch in domain %domain%'), array('domain' => urldecode($domain))); ?></span>
+  <span class="title1"><?php echo pql_complete_constant($LANG->_('Create subbranch in domain %domain%'),
+							array('domain' => $_REQUEST["domain"])); ?></span>
   <br><br>
 <?php
 
 // check if domain exist
-if(!pql_domain_exist($_pql, $domain)) {
-	echo "Domain &quot;$domain&quot; does not exists";
+if(!pql_domain_exist($_pql, $_REQUEST["domain"])) {
+	echo "Domain &quot;".$_REQUEST["domain"]."&quot; does not exists";
 	exit();
 }
 
-if($unit) {
+if($_REQUEST["unit"]) {
     // Check if unit exist
-    if(pql_unit_exist($_pql->ldap_linkid, $domain, $unit)) {
+    if(pql_unit_exist($_pql->ldap_linkid, $_REQUEST["domain"], $_REQUEST["unit"])) {
 	$msg = urlencode($LANG->_('This sub unit already exists'));
 	header("Location: home.php?msg=$msg");
     }
     
-    if(pql_unit_add($_pql->ldap_linkid, $domain, $unit)) {
+    if(pql_unit_add($_pql->ldap_linkid, $_REQUEST["domain"], $_REQUEST["unit"])) {
 	// Redirect to domain-details
-	$msg = urlencode(pql_complete_constant($LANG->_('Sub unit %unit% successfully created'), array("unit" => $unit)));
+	$msg = urlencode(pql_complete_constant($LANG->_('Sub unit %unit% successfully created'), array("unit" => $_REQUEST["unit"])));
 
 	if(pql_get_define("PQL_CONF_AUTO_RELOAD"))
 	  $rlnb = "&rlnb=1";
 
-	header("Location: domain_detail.php?rootdn=$rootdn&domain=$domain&msg=$msg$rlnb");
+	header("Location: domain_detail.php?rootdn=".$url["rootdn"]."&domain=".$url["domain"]."&msg=$msg$rlnb");
     } else {
 	$msg = urlencode($LANG->_('Failed to create the sub unit') . ":&nbsp;" . ldap_error($_pql->ldap_linkid));
 	header("Location: home.php?msg=$msg");
@@ -52,8 +57,8 @@ if($unit) {
       </th>
     </table>
 
-    <input type="hidden" name="rootdn" value="<?=urlencode($rootdn)?>">
-    <input type="hidden" name="domain" value="<?=urlencode($domain)?>">
+    <input type="hidden" name="rootdn" value="<?=$url["rootdn"]?>">
+    <input type="hidden" name="domain" value="<?=$url["domain"]?>">
   </form>
 <?php
 }
