b22f562c682547c416d2185b4fdd88cd0901d0a2
turbo <turbo>
2004-04-02 09:04:19 +0000

    Only show the (URL decoded) first part of the USER_HOST session variable (the actual server name).

--- domain_add_form.php
+++ domain_add_form.php
@@ -1,6 +1,6 @@
 <?php
 // Input form page to create a domain branch in database
-// $Id: domain_add_form.php,v 1.20 2004-03-11 18:13:32 turbo Exp $
+// $Id: domain_add_form.php,v 1.21 2004-04-02 09:04:19 turbo Exp $
 //
 session_start();
 require("./include/pql_config.inc");
@@ -8,8 +8,10 @@ require("./include/pql_config.inc");
 include("./header.html");
 
 $_pql = new pql($_SESSION["USER_HOST"], $_SESSION["USER_DN"], $_SESSION["USER_PASS"]);
+$server = split(';', $_SESSION["USER_HOST"]);
+$server = urldecode($server[0]);
 ?>
-  <span class="title1"><?php echo pql_complete_constant($LANG->_('Create domain branch in LDAP server %server%'), array('server' => $_SESSION["USER_HOST"])); ?></span>
+  <span class="title1"><?php echo pql_complete_constant($LANG->_('Create domain branch in LDAP server %server%'), array('server' => $server)); ?></span>
 
   <br><br>
 
