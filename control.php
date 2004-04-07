a52115069a66c6d280b05951ad293fe88a4eac61
turbo <turbo>
2004-04-07 09:13:22 +0000

    Require the pql_config.inc file to avoid PHP errors.

--- control.php
+++ control.php
@@ -1,8 +1,8 @@
 <?php
 // show details of control
-// $Id: control.php,v 2.2 2003-11-14 11:55:52 turbo Exp $
+// $Id: control.php,v 2.3 2004-04-07 09:13:22 turbo Exp $
 //
-
+require("./include/pql_config.inc");
 include("./header.html");
 ?>
 <span class="title1">Server Control</span>
