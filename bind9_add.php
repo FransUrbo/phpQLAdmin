f5d64aa5b4a9a603096609c2cb9880e9aec0b746
turbo <turbo>
2004-12-19 10:12:28 +0000

    Deglobalize ('$_REQUEST["variable"]' instead of just '$variable').

--- bind9_add.php
+++ bind9_add.php
@@ -1,6 +1,6 @@
 <?php
 // add a domain to a bind9 ldap db
-// $Id: bind9_add.php,v 2.11.16.1 2004-11-15 10:27:17 turbo Exp $
+// $Id: bind9_add.php,v 2.11.16.2 2004-12-19 10:12:28 turbo Exp $
 //
 session_start();
 require("./include/pql_config.inc");
@@ -9,14 +9,14 @@ require("./include/pql_bind9.inc");
 
 include("./header.html");
 
-if($domainname) {
+if($_REQUEST["domainname"]) {
 	$_pql = new pql($_SESSION["USER_HOST"], $_SESSION["USER_DN"], $_SESSION["USER_PASS"]);
 }
 
-if(($action == 'add') and ($type == 'domain')) {
-	  if(!$domainname) {
+if(($_REQUEST["action"] == 'add') and ($_REQUEST["type"] == 'domain')) {
+	  if(!$_REQUEST["domainname"]) {
 ?>
-  <span class="title1"><?php echo pql_complete_constant($LANG->_('Create a DNS zone in branch %domain%'), array('domain' => $domain)); ?></span>
+  <span class="title1"><?php echo pql_complete_constant($LANG->_('Create a DNS zone in branch %domain%'), array('domain' => $_REQUEST["domain"])); ?></span>
 
   <br><br>
 
@@ -38,37 +38,37 @@ if(($action == 'add') and ($type == 'domain')) {
     <input type="submit" value="<?php echo "--&gt;&gt;"; ?>">
   </form>
 <?php } else {
-		  if(pql_bind9_add_zone($_pql->ldap_linkid, $domain, $domainname))
-			$msg = "Successfully added domain $domainname";
+		  if(pql_bind9_add_zone($_pql->ldap_linkid, $_REQUEST["domain"], $_REQUEST["domainname"]))
+			$msg = "Successfully added domain ".$_REQUEST["domainname"];
 		  else
-			$msg = "Failed to add domain $domainname";
+			$msg = "Failed to add domain ".$_REQUEST["domainname"];
 
 		  $url = "domain_detail.php?rootdn=".$_REQUEST["rootdn"]."&domain=".$_REQUEST["domain"]".&view=".$_REQUEST["view"]."&msg=".urlencode($msg);
 		  header("Location: ".pql_get_define("PQL_CONF_URI") . "$url");
 	  }
-} elseif(($action == 'add') and ($type == 'host')) {
-	  if(!$hostname or !$record_type or !$dest) {
-		  if(!$submit) {
+} elseif(($_REQUEST["action"] == 'add') and ($_REQUEST["type"] == 'host')) {
+	  if(!$_REQUEST["hostname"] or !$_REQUEST["record_type"] or !$_REQUEST["dest"]) {
+		  if(!$_REQUEST["submit"]) {
 			  $error_text = array();
 			  $error = false;
 			  
-			  if(!$record_type) {
+			  if(!$_REQUEST["record_type"]) {
 				  $error = true;
 				  $error_text["record_type"] = $LANG->_('Record type missing');
 			  }
 			  
-			  if(!$hostname) {
+			  if(!$_REQUEST["hostname"]) {
 				  $error = true;
 				  $error_text["hostname"] = $LANG->_('Hostname missing');
 			  }
 
-			  if(!$dest) {
+			  if(!$_REQUEST["dest"]) {
 				  $error = true;
 				  $error_text["dest"] = $LANG->_('Resource destination missing');
 			  }
 		  }
 ?>
-  <span class="title1"><?php echo pql_complete_constant($LANG->_('Add a record to domain %domain%'), array('domain' => pql_maybe_idna_decode($domainname))); ?></span>
+  <span class="title1"><?php echo pql_complete_constant($LANG->_('Add a record to domain %domain%'), array('domain' => pql_maybe_idna_decode($_REQUEST["domainname"]))); ?></span>
 
   <br><br>
 
@@ -109,60 +109,60 @@ if(($action == 'add') and ($type == 'domain')) {
 <?php	  } ?>
         <tr class="<?php pql_format_table(); ?>">
           <td class="title">Host name</td>
-          <td><input type="text" name="hostname" value="<?=$hostname?>" size="15"></td>
+          <td><input type="text" name="hostname" value="<?=$_REQUEST["hostname"]?>" size="15"></td>
           <td>
             <select name="record_type">
               <option value="">Please select record type</option>
-              <option value="a" <?php if($record_type == 'a') { echo "SELECTED"; } ?>>A</option>
-              <option value="cname" <?php if($record_type == 'cname') { echo "SELECTED"; } ?>>CNAME</option>
-              <option value="hinfo" <?php if($record_type == 'hinfo') { echo "SELECTED"; } ?>>HINFO</option>
-              <option value="mx" <?php if($record_type == 'mx') { echo "SELECTED"; } ?>>MX</option>
-              <option value="ns" <?php if($record_type == 'ns') { echo "SELECTED"; } ?>>NS</option>
+              <option value="a" <?php if($_REQUEST["record_type"] == 'a') { echo "SELECTED"; } ?>>A</option>
+              <option value="cname" <?php if($_REQUEST["record_type"] == 'cname') { echo "SELECTED"; } ?>>CNAME</option>
+              <option value="hinfo" <?php if($_REQUEST["record_type"] == 'hinfo') { echo "SELECTED"; } ?>>HINFO</option>
+              <option value="mx" <?php if($_REQUEST["record_type"] == 'mx') { echo "SELECTED"; } ?>>MX</option>
+              <option value="ns" <?php if($_REQUEST["record_type"] == 'ns') { echo "SELECTED"; } ?>>NS</option>
             </select>
           </td>
-          <td><input type="text" name="dest" value="<?=$dest?>" size="20"></td>
+          <td><input type="text" name="dest" value="<?=$_REQUEST["dest"]?>" size="20"></td>
         </tr>
       </th>
     </table>
 
-    <input type="hidden" name="submit" value="0">
-    <input type="hidden" name="action" value="add">
-    <input type="hidden" name="type" value="host">
-    <input type="hidden" name="domain" value="<?=$domain?>">
-    <input type="hidden" name="rootdn" value="<?=$rootdn?>">
-    <input type="hidden" name="domainname" value="<?=$domainname?>">
+    <input type="hidden" name="submit"     value="0">
+    <input type="hidden" name="action"     value="add">
+    <input type="hidden" name="type"       value="host">
+    <input type="hidden" name="domain"     value="<?=$_REQUEST["domain"]?>">
+    <input type="hidden" name="rootdn"     value="<?=$_REQUEST["rootdn"]?>">
+    <input type="hidden" name="domainname" value="<?=$_REQUEST["domainname"]?>">
     <br>
     <input type="submit" value="Save">
   </form>
 <?php } else {
-		  $entry[pql_get_define("PQL_ATTR_RELATIVEDOMAINNAME")]	= pql_maybe_idna_encode($hostname);
-		  $entry[pql_get_define("PQL_ATTR_ZONENAME")]				= $domainname;
+		  $entry[pql_get_define("PQL_ATTR_RELATIVEDOMAINNAME")]	= pql_maybe_idna_encode($_REQUEST["hostname"]);
+		  $entry[pql_get_define("PQL_ATTR_ZONENAME")]			= $_REQUEST["domainname"];
 		  $entry[pql_get_define("PQL_ATTR_DNSTTL")]				= 604800;
-		  switch($record_type) {
+		  switch($_REQUEST["record_type"]) {
 			case "a":
-			  $entry[pql_get_define("PQL_ATTR_ARECORD")]			= pql_maybe_idna_encode($dest);
+			  $entry[pql_get_define("PQL_ATTR_ARECORD")]		= pql_maybe_idna_encode($_REQUEST["dest"]);
 			  break;
 			case "cname":
-			  $entry[pql_get_define("PQL_ATTR_CNAMERECORD")]		= pql_maybe_idna_encode($dest);
+			  $entry[pql_get_define("PQL_ATTR_CNAMERECORD")]	= pql_maybe_idna_encode($_REQUEST["dest"]);
 			  break;
 			case "hinfo":
-			  $entry[pql_get_define("PQL_ATTR_HINFORECORD")]		= $dest;
+			  $entry[pql_get_define("PQL_ATTR_HINFORECORD")]	= $_REQUEST["dest"];
 			  break;
 			case "mx":
-			  $entry[pql_get_define("PQL_ATTR_MXRECORD")]			= pql_maybe_idna_encode($dest);
+			  $entry[pql_get_define("PQL_ATTR_MXRECORD")]		= pql_maybe_idna_encode($_REQUEST["dest"]);
 			  break;
 			case "ns":
-			  $entry[pql_get_define("PQL_ATTR_NSRECORD")]			= pql_maybe_idna_encode($dest);
+			  $entry[pql_get_define("PQL_ATTR_NSRECORD")]		= pql_maybe_idna_encode($_REQUEST["dest"]);
 			  break;
 		  }
 
-		  if(pql_bind9_add_host($_pql->ldap_linkid, $domain, $entry))
-			$msg = "Successfully added host <u>$hostname.".pql_maybe_idna_decode($domainname).".</u>";
+		  if(pql_bind9_add_host($_pql->ldap_linkid, $_REQUEST["domain"], $entry))
+			$msg = "Successfully added host <u>".$_REQUEST["hostname"].".".pql_maybe_idna_decode($_REQUEST["domainname"]).".</u>";
 		  else
-			$msg = "Failed to add $hostname to $domainname";
+			$msg = "Failed to add ".$_REQUEST["hostname"]." to ".$_REQUEST["domainname"];
 
 		  $msg = urlencode($msg);
-		  $url = "domain_detail.php?rootdn=$rootdn&domain=$domain&view=dnszone&msg=$msg";
+		  $url = "domain_detail.php?rootdn=".$_REQUEST["rootdn"]."&domain=".$_REQUEST["domain"]."&view=dnszone&msg=$msg";
 
 		  header("Location: $url");
 	  }
