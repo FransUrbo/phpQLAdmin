1290a8a1a88f5ae2ecfef3f6b719806dc24e9f54
turbo <turbo>
2005-03-01 09:38:34 +0000

    Completely remove all support for my own Apache web server object schema hack! I'll create objects for Brian Ferris' module instead (even if I can't get it to work). + This lead to the define 'PQL_ATTR_WEBSRV_OC' becoming an array, which needs   to be de-arrayed in all places where we need it (search filters etc). + Remove all web* attribute and object classe definitions from my schema.

--- scripts/extract_webserver_objects.pl
+++ scripts/extract_webserver_objects.pl
@@ -5,7 +5,7 @@ $LDAP_SERVER="-H ldapi://%2fvar%2frun%2fslapd%2fldapi.main";
 $LDAP_BASEDN="ou=WEB,o=Bayour.COM,c=SE";
 $LDAP_PARAMS="-Y GSSAPI";
 
-$cmd = "$LDAP_SEARCH $LDAP_PARAMS $LDAP_SERVER -b '$LDAP_BASEDN' '(&(objectClass=phpQLAdminWebSrv)(cn=*))'";
+$cmd = "$LDAP_SEARCH $LDAP_PARAMS $LDAP_SERVER -b '$LDAP_BASEDN' '(&(objectClass=ApacheVirtualHostObj)(cn=*))'";
 open(LDAPSEARCH, "$cmd |") || die("Can't open $cmd, $!\n");
 
 # ----------------------------------
@@ -29,28 +29,31 @@ close(LDAPSEARCH);
 # ----------------------------------
 # Create an array which is sorted/ordered by server object
 for($i = 0; $i < $count; $i++) {
-    # dn: cn=cacti.bayour.com,ou=WEB,o=Bayour.COM,c=SE
-    # webServerIP: 212.214.70.54
-    # webServerURL: http://cacti.bayour.com/
-    # webServerAdmin: turbo@bayour.com
-    # webDocumentRoot: /usr/share/cacti/
-    # webServerName: cacti.bayour.com
-    # objectClass: phpQLAdminWebSrv
-    # cn: cacti.bayour.com
-    # webLogError: /var/log/apache/cacti_error.log
-    # webLogTransfer: /var/log/apache/cacti-access.log
-    # webOptions: Include /etc/cacti/apache.conf
+    # dn: cn=testing.bayour.com,ou=WEB,o=Bayour.COM,c=SE
+    # objectClass: device
+    # objectClass: ApacheSectionObj
+    # objectClass: ApacheVirtualHostObj
+    # objectClass: ApacheModLogConfigObj
+    # cn: testing.bayour.com
+    # ApacheSectionName: VirtualHost
+    # ApacheSectionArg: 192.168.1.4
+    # ApacheServerName: testing.bayour.com
+    # ApacheDocumentRoot: /var/www/testing
+    # ApacheServerAdmin: turbo@bayour.com
+    # ApacheErrorLog: /var/log/apache/testing_error.log
+    # ApacheTransferLog: /var/log/apache/testing_access.log
 
     if($line[$i]) {
 	if($line[$i] =~ /^dn: cn=/) {
 	    $dn = (split(' ', $line[$i]))[1];
 	} else {
-	    $attrib = (split(':', $line[$i]))[0];
+	    $attrib =  (split(':', $line[$i]))[0];
+	    $attrib =~ s/^Apache//;
 
 	    $value =  $line[$i];
-	    $value =~ s/$attrib: //;
-	    
-	    if($attrib =~ /webServerIP/) {
+	    $value =~ s/Apache$attrib: //;
+
+	    if($attrib =~ /ServerName/) {
 		$HOSTS{$value} = $value;
 	    }
 	    
@@ -68,14 +71,14 @@ print "\n";
 
 # Create the virtual host directives for Apache.
 foreach $dn (keys %SERVER) {
-    print "<VirtualHost $SERVER{$dn}{webServerIP}>\n";
+    print "<VirtualHost $SERVER{$dn}{SectionArg}>\n";
 
-    print "    ServerName $SERVER{$dn}{cn}\n" if($SERVER{$dn}{cn});
-    print "    DocumentRoot $SERVER{$dn}{webDocumentRoot}\n" if($SERVER{$dn}{webDocumentRoot});
-    print "    ServerAdmin $SERVER{$dn}{webServerAdmin}\n" if($SERVER{$dn}{webServerAdmin});
-    print "    ErrorLog $SERVER{$dn}{webLogError}\n" if($SERVER{$dn}{webLogError});
-    print "    TransferLog $SERVER{$dn}{webLogTransfer}\n" if($SERVER{$dn}{webLogTransfer});
-    print "    $SERVER{$dn}{webOptions}\n" if($SERVER{$dn}{webOptions});
+    print "    ServerName $SERVER{$dn}{ServerName}\n" if($SERVER{$dn}{ServerName});
+    print "    DocumentRoot $SERVER{$dn}{DocumentRoot}\n" if($SERVER{$dn}{DocumentRoot});
+    print "    ServerAdmin $SERVER{$dn}{ServerAdmin}\n" if($SERVER{$dn}{ServerAdmin});
+    print "    ErrorLog $SERVER{$dn}{ErrorLog}\n" if($SERVER{$dn}{ErrorLog});
+    print "    TransferLog $SERVER{$dn}{TransferLog}\n" if($SERVER{$dn}{TransferLog});
+    print "    $SERVER{$dn}{Options}\n" if($SERVER{$dn}{Options});
 
     print "</VirtualHost>\n\n";
 }
