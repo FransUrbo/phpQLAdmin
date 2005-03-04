f40de64ce8a48e2f92b087ea35e5e6cd9260dd37
turbo <turbo>
2005-03-04 09:22:11 +0000

    Don't include the phpQLAdmin API's etc. The are (for the momement) not needed so they just give additional overhead and are in the way...

--- control.php
+++ control.php
@@ -1,9 +1,9 @@
 <?php
 // show details of control
-// $Id: control.php,v 2.4 2005-02-24 17:04:00 turbo Exp $
+// $Id: control.php,v 2.5 2005-03-04 09:22:11 turbo Exp $
 //
-require("./include/pql_config.inc");
-include($_SESSION["path"]."/header.html");
+//require("./include/pql_config.inc");
+//include($_SESSION["path"]."/header.html");
 ?>
 <span class="title1">Server Control</span>
 <br>
