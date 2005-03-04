af819e943d290d9beedd71e0b93e95857673c175
turbo <turbo>
2005-03-04 11:59:58 +0000

    [Merge from HEAD]: Session was started with the path specified in php.ini:session.cookie_path which overwrote session from other installation(s).

--- domain_add_form.php
+++ domain_add_form.php
@@ -1,8 +1,8 @@
 <?php
 // Input form page to create a domain branch in database
-// $Id: domain_add_form.php,v 1.23 2005-02-24 17:04:00 turbo Exp $
+// $Id: domain_add_form.php,v 1.23.2.1 2005-03-04 11:59:44 turbo Exp $
 //
-session_start();
+require("./include/pql_session.inc");
 require("./include/pql_config.inc");
 
 include($_SESSION["path"]."/header.html");
