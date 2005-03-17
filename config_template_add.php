0f98e0f115271f7f45ae0d462845fa1341f73994
turbo <turbo>
2005-03-17 08:23:07 +0000

    After using header() one MUST (!) use 'exit' so that the following code isn't executed! Had no idea, just saw it in the manuall. Replace all calls to header() with my own wrapper that does just that - Calls header() (including the URI session value) and then 'exit'...

--- config_template_add.php
+++ config_template_add.php
@@ -1,6 +1,6 @@
 <?php
 // Add a user template
-// $Id: config_template_add.php,v 2.2.2.1 2005-03-04 11:59:44 turbo Exp $
+// $Id: config_template_add.php,v 2.2.2.2 2005-03-17 08:23:01 turbo Exp $
 //
 // {{{ Setup session etc
 require("./include/pql_session.inc");
@@ -122,7 +122,7 @@ if(($error == 'true')) {
 	echo "If we wheren't debugging (file ./.DEBUG_ME exists), I'd be redirecting you to the url:<br>";
 	die("<b>$url</b>");
   } else
-	header("Location: ".$_SESSION["URI"] . "$url");
+	pql_header($url);
 // }}}
 }
 /*
--- config_template_del.php
+++ config_template_del.php
@@ -1,6 +1,6 @@
 <?php
 // Delete a user template object
-// $Id: config_template_del.php,v 2.2.2.1 2005-03-04 11:59:44 turbo Exp $
+// $Id: config_template_del.php,v 2.2.2.2 2005-03-17 08:23:01 turbo Exp $
 //
 // {{{ Setup session etc
 require("./include/pql_session.inc");
@@ -22,7 +22,7 @@ if(isset($_REQUEST["ok"]) || !pql_get_define("PQL_CONF_VERIFY_DELETE", $_REQUEST
       die();
     }
 
-    header("Location: ".$_SESSION["URI"]."config_detail.php?view=template");
+    pql_header("config_detail.php?view=template");
   }
 // }}}
 } else {
