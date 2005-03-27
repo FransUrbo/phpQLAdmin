7fe307ad3bcb3f6a51777b14edc3dd3d51abb13e
turbo <turbo>
2005-03-27 13:29:52 +0000

    Adding a value (a mail alias for example) for a user and the user have multiple 'cn' values the informational text was 'Array'. Take the first value we have. Closes: 244

--- user_add_attribute.php
+++ user_add_attribute.php
@@ -1,6 +1,6 @@
 <?php
 // adds an attribute 
-// $Id: user_add_attribute.php,v 2.30.2.3 2005-03-19 10:44:52 turbo Exp $
+// $Id: user_add_attribute.php,v 2.30.2.4 2005-03-27 13:29:51 turbo Exp $
 //
 /* This file gets iterated through at least 2 times for any attribute (sequenced by "$submit"):
  *   1) $submit is unset: Set the default value of the attribute (usually from "$oldvalue")
@@ -53,6 +53,9 @@ $username = pql_get_attribute($_pql->ldap_linkid, $_REQUEST["user"], pql_get_def
 if(!$username) {
     // No common name, use uid field
     $username = pql_get_attribute($_pql->ldap_linkid, $_REQUEST["user"], pql_get_define("PQL_ATTR_UID"));
+}
+if($username and is_array($username)) {
+  $username = $username[0];
 }
 
 // select which attribute have to be included
