e6e62316e31b1258e52917ae5d3b087b08ad0f21
turbo <turbo>
2005-03-27 13:31:01 +0000

    Merge from STABLE: Adding a value (a mail alias for example) for a user and the user have multiple 'cn' values the informational text was 'Array'. Take the first value we have.

--- user_add_attribute.php
+++ user_add_attribute.php
@@ -1,6 +1,6 @@
 <?php
 // adds an attribute 
-// $Id: user_add_attribute.php,v 2.35 2005-03-19 15:12:22 turbo Exp $
+// $Id: user_add_attribute.php,v 2.36 2005-03-27 13:31:01 turbo Exp $
 //
 /* This file gets iterated through at least 2 times for any attribute (sequenced by "$submit"):
  *   1) $submit is unset: Set the default value of the attribute (usually from "$oldvalue")
@@ -62,6 +62,9 @@ $username = pql_get_attribute($_pql->ldap_linkid, $_REQUEST["user"], pql_get_def
 if(!$username) {
     // No common name, use uid field
     $username = pql_get_attribute($_pql->ldap_linkid, $_REQUEST["user"], pql_get_define("PQL_ATTR_UID"));
+}
+if($username and is_array($username)) {
+  $username = $username[0];
 }
 
 // {{{ Select which attribute have to be included
