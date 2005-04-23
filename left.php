48cee382fa92750d7827901b2d9b6f4ac3e3684d
turbo <turbo>
2005-04-23 09:04:42 +0000

    * Fix broken CVS id tags * Merge from STABLE:   - Show users in domain/branch if the show_users value is either empty (which     means: 'use default' => 'Yes') or specifically set to 'Yes'.   - The function pql_get_domains() is now doing a better job at sorting the     domains/branches list so no need to call asort().

--- left.php
+++ left.php
@@ -1,6 +1,6 @@
 <?php
 // navigation bar
-// $Id: left.php,v 2.108 2005-03-27 13:20:24 turbo Exp $
+// $Id: left.php,v 2.109 2005-04-23 09:04:42 turbo Exp $
 //
 require("./include/pql_session.inc");
 
@@ -328,7 +328,6 @@ if(!isset($domains)) {
 
   <!-- Domain branches -->
 <?php
-    asort($domains);
     foreach($domains as $key => $domain) {
 	// Get domain part from the DN (Example: 'dc=test,dc=net' => 'test').
 	$d = split(',', urldecode($domain)); $d = split('=', $d[0]); $d = $d[1];
@@ -369,7 +368,9 @@ if(!isset($domains)) {
 	for($i = 0; $branches[$i]; $i++) {
 	    unset($subbranch);
 
-	    if(pql_get_define("PQL_CONF_SHOW_USERS", $rootdn)) {
+	    // Show users is either 'Yes', unset (same thing) or 'No'.
+	    $show_users = pql_get_define("PQL_CONF_SHOW_USERS", $rootdn);
+	    if($show_users or !isset($show_users)) {
 		// Zero out the variables, othervise we won't get users in
 		// specified domain, but also in the PREVIOUS domain shown!
 		unset($users); unset($links); unset($cns);
