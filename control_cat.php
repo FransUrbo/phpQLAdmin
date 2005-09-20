88ed3318f3a068826048c474e6a22be332827f55
turbo <turbo>
2005-09-20 05:25:00 +0000

    * Load the plugin list * Create and generate buttons similar to those that's created on all other   pages (specifically control_detail.php which this file is a part of).

--- control_cat.php
+++ control_cat.php
@@ -1,6 +1,6 @@
 <?php
 // shows details of specified category of attributes
-// $Id: control_cat.php,v 2.21 2005-06-22 13:58:27 turbo Exp $
+// $Id: control_cat.php,v 2.22 2005-09-20 05:25:00 turbo Exp $
 //
 // {{{ Setup session etc
 require("./include/pql_session.inc");
@@ -13,7 +13,13 @@ $_pql_control = new pql_control($_SESSION["USER_HOST"], $_SESSION["USER_DN"], $_
 include($_SESSION["path"]."/header.html");
 // }}}
 
-// {{{ Register all attribute plugins here (require)
+// {{{ Load plugin categories
+require($_SESSION["path"]."/include/pql_control_plugins.inc");
+$cats = pql_plugin_get_cats();
+asort($cats);
+// }}}
+
+// {{{ Load plugin attributes
 $plugins = pql_plugin_get_catplugins($_REQUEST["cat"]);
 if(!is_array($plugins)) {
   die($LANG->_('Invalid category')."!");
@@ -21,10 +27,20 @@ if(!is_array($plugins)) {
 // }}}
 
 // {{{ Setup the buttons
-$buttons = array();
+// ---- First the 'top' buttons (same as on control_detail.php)
+$buttons1 = array('default' => 'Base values');
+foreach($cats as $cat) {
+  $new = array(urlencode($cat) => $cat);
+  $buttons1 = $buttons1 + $new;
+}
+$new = array('action'  => 'Action');
+$buttons1 = $buttons1 + $new;
+
+// ---- Then the plugin attributes
+$buttons2 = array();
 foreach($plugins as $plugin) {
   $new = array($plugin => $plugin);
-  $buttons = $buttons + $new;
+  $buttons2 = $buttons2 + $new;
 }
 // }}}
 
@@ -38,17 +54,29 @@ if(isset($msg)) {
   <br><br>
 <?php
 
-// Output the buttons to the browser
-pql_generate_button($buttons, 'mxhost='.$_REQUEST["mxhost"].'&cat='.urlencode($_REQUEST["cat"])); echo "  <br>\n";
+// {{{ Output the buttons to the browser
+// The fiddeling with the '$_SERVER["PHP_SELF"]' value
+// is because we have to fake it the first time we call
+// pql_generate_button(). It uses the value to setup the
+// URI, and only 'control_detail.php' knows how to convert
+// the 'view=...' option to a 'cat=...' option.
+$tmp = $_SERVER["PHP_SELF"];
+$_SERVER["PHP_SELF"] = eregi_replace('control_cat.php', 'control_detail.php', $_SERVER["PHP_SELF"]);
+pql_generate_button($buttons1, "mxhost=".$_REQUEST["mxhost"]); echo "  <br>\n";
+
+$_SERVER["PHP_SELF"] = $tmp; unset($tmp);
+pql_generate_button($buttons2, 'mxhost='.$_REQUEST["mxhost"].'&cat='.urlencode($_REQUEST["cat"])); echo "  <br>\n";
+// }}}
 
 // {{{ Load the requested control category
-if(!$_REQUEST["view"]) {
+if(@empty($_REQUEST["view"])) {
   $_REQUEST["view"] = $plugins[0];
 }
+// }}}
 
 // {{{ Show the view for this plugin
 if($_REQUEST["view"]) {
-  $file = $_SESSION["path"]."include/".pql_plugin_get_filename($_REQUEST["view"]);
+  $file = $_SESSION["path"]."/"."include/".pql_plugin_get_filename($_REQUEST["view"]);
   include($file);
 
   $func = $_REQUEST["view"] . "_print_view";
