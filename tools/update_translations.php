94d90bae4b2b64117d81d88181cb4b94a53f0e65
turbo <turbo>
2005-09-25 10:57:15 +0000

    * Remove the topmost 'Save Translation' button. Confusing. * Fix/rewrite the 'find -exec' command line. * Prefix each string with a count number (so it's easy to see which string   is being worked on). * HTML code cleanup (newlines and correct tabbing). * To stop PHP warnings when running with 'error_reporting=E_NOTICE':   - Use 'if(empty(array))' instead of 'if(!array[part])' or 'if(!isset(array[part]))'.

--- tools/update_translations.php
+++ tools/update_translations.php
@@ -2,7 +2,7 @@
 // ----------------------------
 // pql_update_translations.php
 //
-// $Id: update_translations.php,v 2.7 2005-09-22 08:45:39 turbo Exp $
+// $Id: update_translations.php,v 2.8 2005-09-25 10:57:15 turbo Exp $
 //
 
 // ----------------------------
@@ -19,8 +19,7 @@ require("../include/pql_session.inc");
 require($_SESSION["path"]."/include/pql_config.inc");
 require($_SESSION["path"]."/include/pql_translations.inc");
 
-$outputFile = "translations/lang.new.inc";
-$baseDir = ".\/"; // were we start searching for strings
+$outputFile = "../translations/lang.new.inc";
 	
 $user_lang = isset($_POST['user_lang']) ? $_POST['user_lang'] : '';
 
@@ -28,11 +27,10 @@ $user_lang = isset($_POST['user_lang']) ? $_POST['user_lang'] : '';
 $os_name = pql_get_os_name();
 if($os_name == "unix") { //matches most unixes
   $pathSeparator = "/";
-  $baseDir = ".\/"; // were we start searching for strings
 } else if($os_name == "windows") {
   $pathSeparator = "\\";
-  $baseDir = "."; // were we start searching for strings
 }
+$baseDir = $_SESSION["path"]; // were we start searching for strings
 ?>
 <html>
   <head>
@@ -85,10 +83,8 @@ if($os_name == "unix") { //matches most unixes
 <form action="<?=$_SERVER["PHP_SELF"]?>" method=POST>
   <input type="hidden" name="mode" value="save">
   <input type="hidden" name="lang" value="<?php echo $lang;?>">
-  <input type="submit" value="Save Translation"><p>
   <textarea rows=10 cols=80 name="toptext">
 <?php echo $file_text;?>
-
 // This is the alphabet of the language. Uppercased characters only at
 // this time. PHP should be able to figure out the lowercased eqvivalent.
 $alphabet = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
@@ -109,29 +105,26 @@ $alphabet = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
 	  $array = grep($files, "/\->_\(\'.*?\'\)/");
 	} else {
 	  // We can use find and grep
-echo "do 'find - exec'<br>";
-	  $return = shell_exec("find $baseDir -follow -name \"*.php\" -exec egrep \"\->_\(\'.*?\'\)\" {} /dev/null \\;");
-	  $array = preg_split("/$baseDir/", $return);
+	  $cmd = "find $baseDir -follow -name '*.php' -o -name '*.inc' -exec grep \"\\\$LANG->_(['\\\"].*['\\\"])\" {} \\;";
+	  $return = `$cmd`;
 	}
-echo "Return: '$return'<br>";
 
-	$array  = preg_split ("/$baseDir/", $return);
+	$array  = preg_split("/\n/", $return);
 	$keys   = array();
 
 	foreach($array as $h) {
-		preg_match_all("/\->_\('(.*?)'\)/", $h, $matches);
-		for($i=1; $i < count($matches); $i++) {
-			if(count($matches[$i]) > 1) {
-				for($j=0; $j < count($matches[$i]); $j++)
-				  $keys[$matches[$i][$j]] = "";
-			} elseif(isset($matches[$i][0]))
-			  $keys[$matches[$i][0]] = "b";
-		}
+	  preg_match_all("/\->_\('(.*?)'\)/", $h, $matches);
+	  for($i=1; $i < count($matches); $i++) {
+	    if(count($matches[$i]) > 1) {
+	      for($j=0; $j < count($matches[$i]); $j++)
+		$keys[$matches[$i][$j]] = "";
+	    } elseif(isset($matches[$i][0]))
+	      $keys[$matches[$i][0]] = "b";
+	  }
 	}
 
 	$keys = array_keys($keys);
 	sort($keys);
-	$count=0;
 ?>
   <span class="title1">Legend:</span><br>
   <font color=red>*</font>&nbsp means a untranslated string<br>
@@ -149,24 +142,44 @@ echo "Return: '$return'<br>";
 
   <table border=1 cellpadding=2 cellspacing=2>
 <?php
+	$count=0; $amount = count($keys) - 2; // Ignore the very first (which is empty) and the last.
+	$amount_len = "%".strlen($amount)."d";
+?>
+    <tr>
+      <td align="center">Nr</td>
+      <td align="left">String to translate</td>
+      <td align="left">Translation</td>
+    </tr>
+
+<?php
+
 	foreach($keys as $key) {
-		if($key != "") {
-			$key = ereg_replace("\\\\'", "'", $key);
-			echo "<tr><td>";
+	  if($key != "") {
+	    $key = ereg_replace("\\\\'", "'", $key);
+	    echo "    <tr>\n";
+	    
+	    printf("      <td align=\"right\">$amount_len</td>\n", $count+1);
 
-			if($language[$key] == "") 
-			  echo "<font color=red>*</font> ";
+	    echo "      <td>\n";
+	    if(empty($language[$key]))
+	      echo "        <font color=red>*</font> ";
+	    
+	    echo $key . "\n";
+	    echo "        <input type=hidden name=\"key_$count\" size=50 value=\"$key\">\n      </td>\n";
+	    if(!empty($language[$key]))
+	      echo "      <td><input type=text name=\"value_$count\" size=50 value=\"$language[$key]\"></td>\n";
+	    else
+	      echo "      <td><input type=text name=\"value_$count\" size=50></td>\n";
 
-			echo $key . "\n";
-			echo "<input type=hidden name=\"key_$count\" size=50 value=\"$key\"></td>";
-			echo "<td><input type=text name=\"value_$count\" size=50 value=\"$language[$key]\"></td></tr>\n";
-			$count++;
-		}
+	    echo "    </tr>\n\n";
+	    $count++;
+	  }
 	}
 ?>
   </table>
 
   <input type="hidden" name="total_keys" value="<?=$count-1?>">
+  <br>
   <input type="submit" value="Save Translation"><p>
 </form>
 <?php } else { 
@@ -192,24 +205,24 @@ echo "Return: '$return'<br>";
 
 	fputs($fp, $header);
 	for($i=0; $i <= $total; $i++) {
-		$key = "key_$i";
-		$val = "value_$i";
-
-		if($i==0)
-			$str = '$language = array("'.$_REQUEST[$key].'" => "'.$_REQUEST[$val].'\"';
-		else
-			$str = '				  "'.$_REQUEST[$key].'" => "'.$_REQUEST[$val].'\"';
-
-		$next = $i+1; $next = "key_$next";
-		if($_REQUEST[$next])
-			$str .= ",\n";
-		else
-			$str .= ");\n\n";
-
-		if(!empty($str)) {
-			$str = stripslashes($str);
-			fputs($fp, $str);
-		}
+	  $key = "key_$i";
+	  $val = "value_$i";
+	  
+	  if($i==0)
+	    $str = '$language = array("'.$_REQUEST[$key].'" => "'.$_REQUEST[$val].'\"';
+	  else
+	    $str = '				  "'.$_REQUEST[$key].'" => "'.$_REQUEST[$val].'\"';
+	  
+	  $next = $i+1; $next = "key_$next";
+	  if(!empty($_REQUEST[$next]))
+	    $str .= ",\n";
+	  else
+	    $str .= ");\n\n";
+	  
+	  if(!empty($str)) {
+	    $str = stripslashes($str);
+	    fputs($fp, $str);
+	  }
 	}
 
 	// Put EMACS mode stuff at the very end...
