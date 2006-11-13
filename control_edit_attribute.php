152c1152ef1119d409594cba5ed05fba02f5285c
turbo <turbo>
2006-11-13 07:11:12 +0000

    Only generate the help table 'header' if the include/attrib.control.*.inc have already been included (via the call_user_func() function). It messed up the debug output and input form.

--- control_edit_attribute.php
+++ control_edit_attribute.php
@@ -1,6 +1,6 @@
 <?php
 // edit an attribute of a control option
-// $Id: control_edit_attribute.php,v 2.33 2005-06-22 13:58:27 turbo Exp $
+// $Id: control_edit_attribute.php,v 2.33.6.1 2006-11-13 07:11:12 turbo Exp $
 //
 // {{{ Setup session etc
 require("./include/pql_session.inc");
@@ -59,20 +59,6 @@ function attribute_forward($msg) {
   <span class="title1">Change control values</span>
   <br><br>
 <?php
-// {{{ Prepare for the help table
-if(function_exists($plugin . "_help")) {
-    // if a help function is available for the plugin,
-    // print additional help table and put the
-    // displayed form of the plugin into a cell of this
-    // table
-?>
-  <table cellspacing="0" cellpadding="3" border="0">
-    <tr>
-
-<?php
-}
-// }}}
-
 // {{{ Select what to do
 if(@$_REQUEST["submit"]) {
     if(call_user_func($plugin . "_check", $_REQUEST["type"])) {
@@ -90,31 +76,32 @@ if(@$_REQUEST["submit"]) {
 if(function_exists($plugin . "_help")) {
 ?>
 
-    <p>
+  <!-- Plugin/Attribute help -->
+  <table cellspacing="0" cellpadding="3" border="0">
+    <tr>
+      <td valign="top">
+        <table cellspacing="0" cellpadding="3" border="0">
+          <th>
+            <tr>
+              <td valign="middle" class="helptitle"><img src="images/help.png" width="16" height="16" border="0">&nbsp;help on this attribute</td>
+            </tr>
 
-    <td valign="top">
-      <table cellspacing="0" cellpadding="3" border="0">
-        <tr>
-          <td valign="middle" class="helptitle"><img src="images/help.png" width="16" height="16" border="0">&nbsp;help on this attribute</td>
-        </tr>
+            <tr>
+              <td height="0" class="helptext" valign="top">
+                <?php call_user_func($plugin . "_help"); ?>
+              </td>
+            </tr>
+<?php if(function_exists($plugin . "_help_cr")) { ?>
 
-        <tr>
-          <td height="0" class="helptext" valign="top">
-            <?php call_user_func($plugin . "_help"); ?>
-          </td>
-        </tr>
-
-<?php if(function_exists($plugin . "_help_cr")) {
-	      // this is the copyright message supplied with help text
-?>
-
-        <tr>
-          <td height="0" class="helptextcr" valign="top">
-            <?php call_user_func($plugin . "_help_cr"); ?>
-          </td>
-        </tr>
+            <tr>
+              <td height="0" class="helptextcr" valign="top">
+                <?php call_user_func($plugin . "_help_cr"); ?>
+              </td>
+            </tr>
 <?php } ?>
-      </table>
+          </th>
+        </table>
+      </td>
     </tr>
   </table>
 <?php
