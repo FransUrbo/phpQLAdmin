03e56fa36499600f6b82f60040c29424f08308de
turbo <turbo>
2006-11-13 13:17:10 +0000

    * Move a lot of the ACL view to tables/host_details-acl.inc (it is used by   host_detail.php as well).   + If called from the branch view->Host ACL, only the users located in     that specific branch is shown in the access list. To get the FULL     list of users that have access to the host, go via 'Computers->[host]'. * Remove the 'Add New Computer' form from host_modify.php. It will be in   a separate file.

--- host_modify.php
+++ host_modify.php
@@ -10,24 +10,15 @@ $filter = pql_get_define("PQL_CONF_REFERENCE_USERS_WITH", $_REQUEST["rootdn"])."
 $users = pql_get_dn($_pql->ldap_linkid, $userdn, $filter);
 
 // Extract 'human readable' name from the user DN's found
-$user_results = pql_left_htmlify_userlist($_pql->ldap_linkid, $_REQUEST["rootdn"], $_REQUEST["domain"],
-										  $userdn, $users, ($links = NULL));
+$user_results = pql_left_htmlify_userlist($_pql->ldap_linkid, $_REQUEST["rootdn"],
+					  $_REQUEST["domain"], $userdn, $users,
+					  ($links = NULL));
 // }}}
 
-// {{{ Retreive all computers
-if(pql_get_define("PQL_CONF_SUBTREE_COMPUTERS")) {
-  $subrdn =  pql_get_define("PQL_CONF_SUBTREE_COMPUTERS") . ",";
-}
-$computerdn = $subrdn . $_GET["domain"];
-$filter = "(& (objectClass=ipHost)(cn=*))";
-$computer_results = pql_search($_pql->ldap_linkid, $computerdn, $filter);
-
-if(is_array($computer_results) and !@$computer_results[0]) {
-  // Make sure it's a numbered array...
-  $tmp = $computer_results;
-  unset($computer_results);
-  $computer_results[] = $tmp;
-}
+// {{{ Retreive all physical computers
+$computer_results = pql_get_dn($_pql->ldap_linkid, $_SESSION["USER_SEARCH_DN_CTR"],
+			       '(&(cn=*)(|(objectclass=ipHost)(objectclass=device)))',
+			       'ONELEVEL');
 // }}}
 
 if(isset($_REQUEST['action']) && ($_REQUEST['action'] == 'remove_user_from_host')) {
@@ -122,12 +113,13 @@ if(isset($_REQUEST['action']) && ($_REQUEST['action'] == 'remove_user_from_host'
 // }}}
 }
 
-if(is_array($computer_results[0])) {
+if(is_array($computer_results)) {
+  // {{{ Show this domains user access
 ?>
 
   <form method="post">
     <table cellspacing="0" cellpadding="3" border="0">
-      <th colspan="3" align="left"><?=$LANG->_("Host Control Access")?></th>
+      <th colspan="3" align="left"><?=$LANG->_("Host Control Access")?>
         <tr class="c2">
           <td class="title"><?=$LANG->_("Allow")?></td>
 <?php
@@ -150,20 +142,24 @@ if(is_array($computer_results[0])) {
     print "            </select>\n          </td>\n";
 // }}}
   }
+
+  // {{{ Host column
 ?>
 
           <td>
             <?=$LANG->_("To Access")."\n"?>
             <select name="computer">
 <?php
-  for($i=0; $i < count($computer_results); $i++) {
-    $host = $computer_results[$i][pql_get_define("PQL_ATTR_CN")];
-    $hostdn = $computer_results[$i]['dn'];
-    print "              <option value='" . $hostdn . "'>" . $host . "</option>\n";
+  foreach($computer_results as $host_dn) {
+    $host = pql_get_attribute($_pql->ldap_linkid, $host_dn, pql_get_define("PQL_ATTR_CN"));
+    print "              <option value='" . $host_dndn . "'>" . $host . "</option>\n";
   }
 ?>
             </select>
           </td>
+<?php
+// }}}
+?>
         </tr>
       </th>
     </table>
@@ -173,165 +169,31 @@ if(is_array($computer_results[0])) {
     <input type="Submit" name="Submit" value="<?=$LANG->_("Add To Host ACL")?>">
   </form>
 
-  <table cellspacing="0" cellpadding="3" border="0">
-    <th colspan="3" align="left">
-      <tr class="title">
-        <td><?=$LANG->_("Hostname")?></td>
-        <td><?=$LANG->_("IP Address")?></td>
-      </tr>
 <?php
-  $row = 'c1';
-  for($i=0; $i < count($computer_results); $i++) {
-    $host   = $computer_results[$i][pql_get_define("PQL_ATTR_CN")];
-    $hostdn = $computer_results[$i]['dn'];
-    $hostIP = $computer_results[$i][pql_get_define("PQL_ATTR_IPHOSTNUMBER")];
-?>	
-      <tr class="<?=$row?>">
-        <!-- HOSTNAME AND USERS -->
-        <td>
-<?php if($_SESSION["opera"]) { ?>
-          <div id="el<?=$i?>aParent" class="parent" onclick="showhide(el<?=$i?>aSpn, el<?=$i?>aImg); showhide(el<?=$i?>bSpn, el<?=$i?>bImg);">
-            <img name="imEx" src="images/minus.png" border="0" alt="-" width="9" height="9" id="el<?=$i?>aImg">
-            <a class="item"><font color="black" class="heada"><?=$host?></font></a>
-          </div>
-<?php } else { ?>
-          <div id="el<?=$i."aParent"?>" class="parent">
-            <a href="<?=$_SERVER['REQUEST_URI']?>" onClick="if (capable) {expandBase('el<?=$i+1?>', true); return false;}">
-              <img name="imEx" src="images/plus.png" border="0" alt="+" width="9" height="9" id="el<?=$i."Img"?>"></a>
-	
-            <font color="black" class="heada"><?=$host?></font>
-          </div>
-<?php }
-
-      $users = $computer_results[$i][pql_get_define('PQL_ATTR_UNIQUE_GROUP')];
-      if(@$users) {
-	if($_SESSION["opera"]) {
-?>
-          <span id="el<?=$i?>aSpn" style="display:''">
-<?php	} else { ?>
-          <div id="<?=$i."aChild"?>" class="child">
-<?php	}
-
-	// {{{ Is there any users?
-	if(is_array($users)) {
-	  for($j=0; $j < count($users); $j++) {
-	    $user = pql_get_attribute($_pql->ldap_linkid, $users[$j],
-				      pql_get_define("PQL_ATTR_UID"));
-?>
-              &nbsp;&nbsp;&nbsp;<img src="images/navarrow.png" width="9" height="9" border="0">&nbsp;<?=$user."\n"?>
-              <a href="<?=$_SERVER['REQUEST_URI']?>&groupdn=<?=$hostdn?>&userdn=<?=$users[$j]?>&action=remove_user_from_host">
-                <img src="images/del.png" width="12" height="12" border="0" alt="<?=pql_complete_constant($LANG->_("Delete %user% from computer"), array('user' => $user))?>"></a>
-              <br>
-<?php	  }
-	} else {
-	  $user = pql_get_attribute($_pql->ldap_linkid, $users,
-				    pql_get_define("PQL_ATTR_UID"));
-?>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <img src="images/navarrow.png" width="9" height="9" border="0">
-              <?=$user?>
-              <a href="<?=$_SERVER['REQUEST_URI']?>&groupdn=<?=$hostdn?>&userdn=<?=$users?>&action=remove_user_from_host">
-                <img src="images/del.png" width="12" height="12" border="0" alt="<?=pql_complete_constant($LANG->_("Delete %user% from computer"), array('user' => $user))?>"></a>
-              <br>
-<?php	} ?>
-              </nobr>
-<?php	if($_SESSION["opera"]) { ?>
-            </span>
-<?php	} else { ?>
-            </div>
-<?php	}
 // }}}
-      }
-?>
-          </td>
-<?php if($hostIP) {
-	// {{{ Is there is an IP address?
-	$host_ip = '';
-	if(is_array($hostIP)) {
-	  for($z = 0; $z < count($hostIP); $z++) {
-	    $host_ip .= $hostIP[$z];
-	    if($hostIP[$z+1])
-	      $host_ip .= ",&nbsp";
-	  }
-	} else
-	  $host_ip = $hostIP;
-
-?>
-
-          <!-- IP ADDRESS -->
-          <td>
-<?php	  if($_SESSION["opera"]) { ?>
-            <div id="el<?=$i?>bParent" class="parent" onclick="showhide(el<?=$i?>aSpn, el<?=$i?>aImg); showhide(el<?=$i?>bSpn, el<?=$i?>bImg);">
-              <img name="imEx" src="images/minus.png" border="0" alt="-" width="9" height="9" id="el<?=$i?>bImg">
-              <a class="item"><font color="black" class="heada"><?=$host_ip?></font></a>
-            </div>
-
-            <span id="el<?=$i?>bSpn" style="display:''">
-<?php	  } else { ?>
-            <div id="el<?=$i?>bParent" class="parent">
-              <a class="item" onClick="if (capable) {expandBase('el<?=$i?>', true); return false;}">
-                <img name="imEx" src="images/plus.png" border="0" alt="+" width="9" height="9" id="el<?=$i?>Img">
-              </a>
-              <a class="item"><font color="black" class="heada"><?=$host_ip?></font></a>
-            </div>
-
-            <div id="el<?=$i?>bChild" class="child">
-<?php	  }
-	  echo "            <br>\n";
-
-	  if($_SESSION["opera"]) {
-	    echo "            </span>\n";
-	  } else {
-	    echo "            </div>\n";
-	  }
-	  
-	  print "          </td>\n        </tr>\n";
-	  
-	  if($row == 'c1')
-	    $row = 'c2';
-	  else
-	    $row = 'c1';
-// }}}
-      }
-  } // for($i=0; $i < count($computer_results); $i++)
-?>
-      </table>
-
-      <br>
-
-<?php
 } else { 
   // {{{ There's no computers, so we couldn't insert the 'add user to host ACL' form above.
   // Instead, just say there wasn't any hosts to modify...
   echo '<img src="images/info.png" width="16" height="16" alt="" border="0">';
   echo "&nbsp;&nbsp;";
-  echo $LANG->_("Could not find any computers/hosts, so I couldn't show you the form. Start by adding a host...");
+  echo $LANG->_("Could not find any computers/hosts, so I couldn't show you the form. Start by adding a host. You do this at the left Computers frame.");
 // }}}
 }
 
-// {{{ Create the 'Add New Computer' form
-?>
-      <form method="post">
-        <table cellspacing="1" cellpadding="3" border="0">
-          <th colspan="3" align="left"><?=$LANG->_("Add New Computer")?>
-            <tr class="c2">
-              <td class="title"><?=$LANG->_("Fully Qualified Domain Name")?></td>
-              <td class="title"><?=$LANG->_("IP Address")?></td>
-            </tr>
+// {{{ Find all host with user from this branch
+// Setup search filter for finding hosts
+// objectClass => ipHost AND groupOfUniqueNames.
+$filter  = '(&(objectClass=ipHost)(objectClass=groupOfUniqueNames)(|';
+foreach($users as $user) {
+  // Search for each and every user in the groupOfUniqueNames.
+  $filter .= '('.pql_get_define("PQL_ATTR_UNIQUE_GROUP").'='.$user.')';
+}
+$filter .= '))';
 
-            <tr>
-              <td><input type="text" name="hostname" size="40"></td>
-              <td><input type="text" name="hostip"   size="20"></td>
-            </tr>
-          </th>
-        </table>
-
-        <input type="hidden" name="view"   value="hostacl">
-        <input type="hidden" name="action" value="add_new_host">
-        <input type="submit" name="Submit" value="<?=$LANG->_('Add New Host')?>">
-      </form>
-<?php
+$hosts = pql_get_dn($_pql->ldap_linkid, $_SESSION["USER_SEARCH_DN_CTR"], $filter);
 // }}}
+
+include("./tables/host_details-acl.inc");
 
 /* Local variables:
  * mode: php
