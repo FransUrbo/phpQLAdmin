b162c9c2313b926ba678a957ddeed8b7044894b3
turbo <turbo>
2007-03-09 17:23:32 +0000

    Include the pql_ppolicy.inc API file.

--- user_edit_attribute.php
+++ user_edit_attribute.php
@@ -1,6 +1,6 @@
 <?php
 // edit an attribute of user
-// $Id: user_edit_attribute.php,v 2.63 2007-02-26 10:50:51 turbo Exp $
+// $Id: user_edit_attribute.php,v 2.64 2007-03-09 17:23:32 turbo Exp $
 //
 // This file gets iterated through at least 2 times for any attribute (sequenced by "$submit"):
 //   1) $submit is unset: Set the default value of the attribute (usually from "$oldvalue")
@@ -14,6 +14,7 @@ require("./include/pql_session.inc");
 require($_SESSION["path"]."/include/pql_config.inc");
 require($_SESSION["path"]."/include/config_plugins.inc");
 require($_SESSION["path"]."/include/pql_krbafs.inc");
+require($_SESSION["path"]."/include/pql_ppolicy.inc");
 
 $url["domain"] = pql_format_urls($_REQUEST["domain"]);
 $url["rootdn"] = pql_format_urls($_REQUEST["rootdn"]);
